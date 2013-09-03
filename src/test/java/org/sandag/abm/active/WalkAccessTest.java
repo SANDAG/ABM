724d963c022a4b0b051937f4ce2fa2210643dd09
Clint Daniels <clint.daniels@sandag.org>
2013-09-03 14:30:01 -0700

    Added unit test. (ABM-169)

---
+++ src/test/java/org/sandag/abm/active/WalkAccessTest.java
@@ -0,0 +1,15 @@
+package org.sandag.abm.active;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class WalkAccessTest
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
