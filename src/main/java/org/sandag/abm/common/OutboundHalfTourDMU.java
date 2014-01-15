b43620cad98aa66b50270aef138672db0b2e35d0
John Mulholland <john.mulholland@rsginc.com>
2014-01-15 13:54:11 -0700

    Refactored, especially for ABM-250

---
+++ src/main/java/org/sandag/abm/common/OutboundHalfTourDMU.java
@@ -0,0 +1,90 @@
+package org.sandag.abm.common;
+
+public abstract class OutboundHalfTourDMU extends DMU {
+    public int                      outboundHalfTourDirection;
+	protected double getTransitSkimFromMethodName(String methodName) throws Exception 
+	{
+    	//getDt_lb_EgrTime
+    	String[] parts = methodName.split("_");
+    	String part1 = parts[0].replace("get", ""); 
+    	String part2 = parts[1];
+    	boolean part3HasUnderscore = (parts.length > 3);
+    	String part3 = parts[2];
+    	if (part3HasUnderscore)
+    	{
+    		part3 += "_" + parts[3];
+    	}
+    	part3 = part3.replace("Time", "").replace("Walk", "");
+    	
+    	int var1 = -1, var2 = -1, var3 = -1, var4 = -1;
+    	
+    	if (part1.equalsIgnoreCase("WTW"))
+    		var1 = WTW;
+    	else if (part1.equalsIgnoreCase("DT"))
+    	{
+    		if (outboundHalfTourDirection == 1)
+    		{
+    			var1 = DTW;
+    		}
+    		else
+    		{
+    			var1 = WTD;		
+    		}
+    	}    	
+    	
+    	else
+    		throw new Exception("First part of getTransitSkim is invalid with variable " + var1);
+    	    	
+    	if (part2.equalsIgnoreCase("LB"))
+    		var2 = LB;
+    	else if (part2.equalsIgnoreCase("EB"))
+    		var2 = EB;
+    	else if (part2.equalsIgnoreCase("BRT"))
+    		var2 = BRT;
+    	else if (part2.equalsIgnoreCase("LR"))
+    		var2 = LR;
+    	else if (part2.equalsIgnoreCase("CR"))
+    		var2 = CR;
+    	else
+    		throw new Exception("Second part of getTransitSkim is invalid with variable " + var1);
+    	
+    	
+    	if (var1 == DTW && part3.equalsIgnoreCase("ACC"))
+    		return 0;
+    	if (var1 == WTD && part3.equalsIgnoreCase("EGR"))
+    		return 0;
+    	if (part3.equalsIgnoreCase("FWAIT"))
+    		var3 = FWAIT;
+    	else if (part3.equalsIgnoreCase("XWAIT"))
+    		var3 = XWAIT;
+    	else if (part3.equalsIgnoreCase("ACC"))
+    		var3 = ACC;
+    	else if (part3.equalsIgnoreCase("EGR"))
+    		var3 = EGR;
+    	else if (part3.equalsIgnoreCase("AUX"))
+    		var3 = AUX;
+    	else if (part3.equalsIgnoreCase("FARE"))
+    		var3 = FARE;
+    	else if (part3.equalsIgnoreCase("XFERS"))
+    		var3 = XFERS;
+    	else if (part3.equalsIgnoreCase("LB_IVT"))
+    		var3 = LB_IVT;
+    	else if (part3.equalsIgnoreCase("EB_IVT"))
+    		var3 = EB_IVT;
+    	else if (part3.equalsIgnoreCase("BRT_IVT"))
+    		var3 = BRT_IVT;
+    	else if (part3.equalsIgnoreCase("LRT_IVT")) //Note difference
+    		var3 = LR_IVT;
+    	else if (part3.equalsIgnoreCase("CR_IVT"))
+    		var3 = CR_IVT;
+    	else if (var1 == DTW && part3.equalsIgnoreCase("DRV"))
+    		var3 = ACC;
+    	else if (var1 == WTD && part3.equalsIgnoreCase("DRV"))
+    		var3 = EGR;
+    	else
+    		throw new Exception("Third part of getTransitSkim is invalid with variable " + var1);
+    	
+    	return getTransitSkim(var1, var2, var3);    	
+	}
+	protected abstract double getTransitSkim(int var1, int var2, int var3);
+}
--- src/main/java/org/sandag/abm/internalexternal/InternalExternalTripModeChoiceDMU.java
+++ src/main/java/org/sandag/abm/internalexternal/InternalExternalTripModeChoiceDMU.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
-import org.sandag.abm.common.DMU;
+import org.sandag.abm.common.OutboundHalfTourDMU;
 import org.sandag.abm.ctramp.McLogsumsCalculator;
 
 import com.pb.common.calculator.IndexValues;
 import com.pb.common.calculator.VariableTable;
 
-public class InternalExternalTripModeChoiceDMU extends DMU
+public class InternalExternalTripModeChoiceDMU extends OutboundHalfTourDMU
         implements Serializable, VariableTable
 {
     protected int                      tourDepartPeriod;
@@ -33,7 +33,6 @@ public class InternalExternalTripModeChoiceDMU extends DMU
     protected double                   nmBikeTime;
 
     protected double[][][]             transitSkim;
-    protected int                      outboundHalfTourDirection;
 
     public InternalExternalTripModeChoiceDMU(InternalExternalModelStructure modelStructure, Logger aLogger)
     {
@@ -503,89 +502,4 @@ public class InternalExternalTripModeChoiceDMU extends DMU
         throw new UnsupportedOperationException();
     }
     
-    protected double getTransitSkimFromMethodName(String methodName) throws Exception 
-	{
-    	//getDt_lb_EgrTime
-    	String[] parts = methodName.split("_");
-    	String part1 = parts[0].replace("get", ""); 
-    	String part2 = parts[1];
-    	boolean part3HasUnderscore = (parts.length > 3);
-    	String part3 = parts[2];
-    	if (part3HasUnderscore)
-    	{
-    		part3 += "_" + parts[3];
-    	}
-    	part3 = part3.replace("Time", "").replace("Walk", "");
-    	
-    	int var1 = -1, var2 = -1, var3 = -1, var4 = -1;
-    	
-    	if (part1.equalsIgnoreCase("WTW"))
-    		var1 = WTW;
-    	else if (part1.equalsIgnoreCase("DT"))
-    	{
-    		if (outboundHalfTourDirection == 1)
-    		{
-    			var1 = DTW;
-    		}
-    		else
-    		{
-    			var1 = WTD;		
-    		}
-    	}    	
-    	
-    	else
-    		throw new Exception("First part of getTransitSkim is invalid with variable " + var1);
-    	    	
-    	if (part2.equalsIgnoreCase("LB"))
-    		var2 = LB;
-    	else if (part2.equalsIgnoreCase("EB"))
-    		var2 = EB;
-    	else if (part2.equalsIgnoreCase("BRT"))
-    		var2 = BRT;
-    	else if (part2.equalsIgnoreCase("LR"))
-    		var2 = LR;
-    	else if (part2.equalsIgnoreCase("CR"))
-    		var2 = CR;
-    	else
-    		throw new Exception("Second part of getTransitSkim is invalid with variable " + var1);
-    	
-    	
-    	if (var1 == DTW && part3.equalsIgnoreCase("ACC"))
-    		return 0;
-    	if (var1 == WTD && part3.equalsIgnoreCase("EGR"))
-    		return 0;
-    	if (part3.equalsIgnoreCase("FWAIT"))
-    		var3 = FWAIT;
-    	else if (part3.equalsIgnoreCase("XWAIT"))
-    		var3 = XWAIT;
-    	else if (part3.equalsIgnoreCase("ACC"))
-    		var3 = ACC;
-    	else if (part3.equalsIgnoreCase("EGR"))
-    		var3 = EGR;
-    	else if (part3.equalsIgnoreCase("AUX"))
-    		var3 = AUX;
-    	else if (part3.equalsIgnoreCase("FARE"))
-    		var3 = FARE;
-    	else if (part3.equalsIgnoreCase("XFERS"))
-    		var3 = XFERS;
-    	else if (part3.equalsIgnoreCase("LB_IVT"))
-    		var3 = LB_IVT;
-    	else if (part3.equalsIgnoreCase("EB_IVT"))
-    		var3 = EB_IVT;
-    	else if (part3.equalsIgnoreCase("BRT_IVT"))
-    		var3 = BRT_IVT;
-    	else if (part3.equalsIgnoreCase("LRT_IVT")) //Note difference
-    		var3 = LR_IVT;
-    	else if (part3.equalsIgnoreCase("CR_IVT"))
-    		var3 = CR_IVT;
-    	else if (var1 == DTW && part3.equalsIgnoreCase("DRV"))
-    		var3 = ACC;
-    	else if (var1 == WTD && part3.equalsIgnoreCase("DRV"))
-    		var3 = EGR;
-    	else
-    		throw new Exception("Third part of getTransitSkim is invalid with variable " + var1);
-    	
-    	return getTransitSkim(var1, var2, var3);    	
-	}
-
 }
