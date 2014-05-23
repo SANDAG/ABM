feb5358c2969997d99056f65ac60121529c8e69b
Clint Daniels <clint.daniels@sandag.org>
2014-05-22 17:28:32 -0700

    Clean up network mapping. (ABM-402)

--- build_all.cmd
+++ build_all.cmd
@@ -2,21 +2,25 @@ e:
 cd \apps\ABM
 
 call mvn package -P aztec,2010 -DskipTests
+call mvn package -P aztec,2012 -DskipTests
 call mvn package -P aztec,2020 -DskipTests
 call mvn package -P aztec,2035 -DskipTests
 call mvn package -P aztec,2050 -DskipTests
 
 call mvn package -P charger,2010 -DskipTests
+call mvn package -P charger,2012 -DskipTests
 call mvn package -P charger,2020 -DskipTests
 call mvn package -P charger,2035 -DskipTests
 call mvn package -P charger,2050 -DskipTests
 
 call mvn package -P mustang,2010 -DskipTests
+call mvn package -P mustang,2012 -DskipTests
 call mvn package -P mustang,2020 -DskipTests
 call mvn package -P mustang,2035 -DskipTests
 call mvn package -P mustang,2050 -DskipTests
 
 call mvn package -P gaucho,2010 -DskipTests
+call mvn package -P gaucho,2012 -DskipTests
 call mvn package -P gaucho,2020 -DskipTests
 call mvn package -P gaucho,2035 -DskipTests
 call mvn package -P gaucho,2050 -DskipTests
