397865bd140fd1695fae912e9d9f5b0291f7f706
alsuprm <alsuprm@pbworld.com>
2015-03-02 09:02:22 -0800

    Added post-processing to create DTA trip list (DTA-167)

---
+++ src/main/java/org/sandag/abm/dta/postprocessing/dtaTrip.java
@@ -0,0 +1,383 @@
+package org.sandag.abm.dta.postprocessing;
+
+import java.io.PrintWriter;
+import java.io.Serializable;
+
+public class dtaTrip implements Serializable {
+
+	private int id;
+    private int hhid;
+    private int persid;
+    private int tourid;
+    private int originTaz;
+    private int destinationTaz;
+    private int originMGRA;
+    private int destinationMGRA;
+    private int originNode;
+    private int destinationNode;
+    private String vehicleType;
+    private int vehOccupancy;
+    private int tollEligible;
+    private String marketSegment;
+    private int broadPeriod;
+    private int detailedPeriod;
+    private int dtaPeriod;
+    private int driver;
+    private double expansionFactor;
+
+    	
+       
+    /**
+    * Default constructor; nothing initialized.
+    */
+    public dtaTrip(){
+        	
+    }
+    /**
+     * Initialize a trip will zero values for all fields
+     */
+    public void initializeTrip() {
+       	this.id=0;
+       	this.hhid=0;
+       	this.persid=0;
+       	this.tourid=0;
+       	this.originTaz=0;
+       	this.destinationTaz=0;
+       	this.originMGRA=0;
+       	this.destinationMGRA=0;
+       	this.originNode=0;
+       	this.destinationNode=0;
+       	this.vehicleType="na";
+       	this.vehOccupancy=0;
+       	this.tollEligible=0;
+       	this.marketSegment="na";
+       	this.broadPeriod=0;
+       	this.detailedPeriod=0;
+       	this.dtaPeriod=0;
+       	this.driver=-1;
+       	this.expansionFactor=1.0;
+        			
+    }
+    /**
+     * @return the household id
+     */
+    public int getHHId() {
+    	return hhid;
+    }
+    /**
+     * @param hhid the household id to set
+     */
+    public void setHHId(int hhid) {
+    	this.hhid = hhid;
+    }
+    /**
+     * @return the person id
+     */
+    public int getPersonId() {
+    	return persid;
+    }
+    /**
+     * @param persid the person id to set
+     */
+    public void setPersonId(int persid) {
+    	this.persid = persid;
+    }
+    /**
+     * @return the tour id
+     */
+    public int getTourId() {
+    return tourid;
+    }
+    /**
+     * @param tourid the tour id to set
+     */
+    public void setTourId(int tourid) {
+    	this.tourid = tourid;
+    }                         
+    /**
+     * @return the id
+     */
+    public int getId() {
+    	return id;
+    }
+   	/**
+   	 * @param id the id to set
+   	 */
+   	public void setId(int id) {
+   		this.id = id;
+   	}
+   	
+   	/**
+   	 * @return the originTaz
+   	 */
+   	public int getOriginTaz() {
+   		return originTaz;
+   	}
+   	/**
+   	 * @param originTaz the originTaz to set
+   	 */
+   	public void setOriginTaz(int originTaz) {
+   		this.originTaz = originTaz;
+   	}
+   	/**
+   	 * @return the destinationTaz
+   	 */
+   	public int getDestinationTaz() {
+   		return destinationTaz;
+   	}
+   	/**
+   	 * @param destinationTaz the destinationTaz to set
+   	 */
+   	public void setDestinationTaz(int destinationTaz) {
+   		this.destinationTaz = destinationTaz;
+   	}
+   	
+   	/**
+   	 * @return the originMGRA
+   	 */
+   	public int getOriginMGRA() {
+   		return originMGRA;
+   	}
+   	/**
+   	 * @param originMGRA the originMGRA to set
+   	 */
+   	public void setOriginMGRA(int originMGRA) {
+   		this.originMGRA = originMGRA;
+   	}
+   	/**
+   	 * @return the destinationMGRA
+   	 */
+   	public int getDestinationMGRA() {
+   		return destinationMGRA;
+   	}
+   	/**
+   	 * @param destinationMGRA the destinationMGRA to set
+   	 */
+   	public void setDestinationMGRA(int destinationMGRA) {
+   		this.destinationMGRA = destinationMGRA;
+   	}
+   	
+   	/**
+   	 * @return the originNode
+   	 */
+   	public int getOriginNode() {
+   		return originNode;
+   	}
+   	/**
+   	 * @param originNode the originNode to set
+   	 */
+   	public void setOriginNode(int originNode) {
+   		this.originNode = originNode;
+   	}
+   	/**
+   	 * @return the destinationNode
+   	 */
+   	public int getDestinationNode() {
+   		return destinationNode;
+   	}
+   	/**
+   	 * @param destinationMGRA the destinationMGRA to set
+   	 */
+   	public void setDestinationNode(int destinationNode) {
+   		this.destinationNode = destinationNode;
+   	}
+   	/**
+   	 * set trip mode values based on trip mode in input file
+   	 */
+   	public void setTripMode(int mode) {
+   		if(mode<=8||mode>=27){
+   			setVehicleType("passengerCar");
+			setVehicleOccupancy(1);
+   			setTollEligible(0);
+   			if(mode==2){
+   				setTollEligible(1);
+   			}
+   			if((mode>=3 && mode<=5)||mode==27){
+   				setVehicleOccupancy(2);
+   			}
+   			if(mode==5){
+   				setTollEligible(1);
+   			}
+   			if(mode>=6 && mode<=8){
+   				setVehicleOccupancy(3);
+   			}
+   			if(mode==8){
+   				setTollEligible(1);   				
+   			}
+   		}
+   		if(mode>8 && mode<11){
+   			setVehicleType("nonMotorized");
+   			setVehicleOccupancy(0);
+   			setTollEligible(0);
+   		}
+   		if(mode>=11 && mode<16){
+   			setVehicleType("WalkTransit");
+   			setVehicleOccupancy(0);
+   			setTollEligible(0);
+   		}
+   		if(mode>=16 && mode<26){
+   			setVehicleType("DriveTransit");
+   			setVehicleOccupancy(1);
+   			setTollEligible(0);
+   		}
+   	}
+   	/**
+   	 * @return the person number of the driver
+   	 */
+   	public int getTourDriver() {
+   		return driver;
+   	}
+   	/**
+   	 * @param driver the tour driver to set
+   	 */
+   	public void setTourDriver(int tourDriver) {
+   		this.driver = tourDriver;
+   	}
+   	
+   	/**
+   	 * @return the vehicleType
+   	 */
+   	public String getVehicleType() {
+   		return vehicleType;
+   	}
+   	/**
+   	 * @param vehicleType the vehicleType to set
+   	 */
+   	public void setVehicleType(String vehicleType) {
+   		this.vehicleType = vehicleType;
+   	}
+   	/**
+   	 * @return the vehicleOccupancy
+   	 */
+   	public int getVehicleOccupancy() {
+   		return vehOccupancy;
+   	}
+   	/**
+   	 * @param vecOccupancy the vehOccupancy to set
+   	 */
+   	public void setVehicleOccupancy(int vehOccupancy) {
+   		this.vehOccupancy = vehOccupancy;
+   	}
+   	/**
+   	 * @return the tollEligibility
+   	 */
+   	public int getTollEligible() {
+   		return tollEligible;
+   	}
+   	/**
+   	 * @param tollEligible the tollEligible to set
+   	 */
+   	public void setTollEligible(int tollEligible) {
+   		this.tollEligible = tollEligible;
+   	}
+   	/**
+   	 * @return the market segment
+   	 */
+   	public String getMarketSegment() {
+   		return marketSegment;
+   	}
+   	
+   	/**
+   	 * @param marketSegment the marketSegment to set
+   	 */
+   	public void setMarketSegment(String marketSegment){
+   		this.marketSegment = marketSegment;
+   	}
+    	
+   	/**
+   	 * @return the broad time period
+   	 */
+   	public int getBroadPeriod() {
+   		return broadPeriod;
+   	}
+   	/**
+   	 * @param broadPeriod the broadPeriod to set
+   	 */
+   	public void setBroadPeriod(int Period) {
+   		this.broadPeriod = Period;
+   	}
+
+   	/**
+   	 * @return the detailed time period
+   	 */
+   	public int getDetailedPeriod() {
+   		return detailedPeriod;
+   	}
+   	/**
+   	 * @param detailedPeriod the detailedPeriod to set
+   	 */
+   	public void setDetailedPeriod(int Period) {
+   		this.detailedPeriod = Period;
+   	}
+
+   	/**
+   	 * @return the dta time period
+   	 */
+   	public int getDTAPeriod() {
+   		return dtaPeriod;
+   	}
+   	/**
+   	 * @param dtaPeriod the dtaPeriod to set
+   	 */
+   	public void setDTAPeriod(int Period) {
+   		this.dtaPeriod = Period;
+   	}
+   	/**
+   	 * @return the trip expansion factor
+   	 */
+   	public double getExpansionFactor() {
+   		return expansionFactor;
+   	}
+   	/**
+   	 * @param dtaPeriod the dtaPeriod to set
+   	 */
+   	public void setExpansionFactor(double expansionFactor) {
+   		this.expansionFactor = expansionFactor;
+   	}
+
+    /**
+     * Write the trip
+     * 
+     * @param writer
+     */
+    public void writeTrip(PrintWriter writer){
+     	String record = new String(
+       	   hhid + "," +
+       	   persid + "," +
+       	   tourid + "," +
+       	   id + "," +
+       	   originTaz + "," +
+       	   destinationTaz + "," +
+       	   originMGRA + "," +
+       	   destinationMGRA + "," +
+       	   originNode + "," +
+       	   destinationNode + "," +
+       	   vehicleType + "," +
+       	   vehOccupancy + "," +
+       	   tollEligible + "," +
+       	   marketSegment + "," +
+       	   detailedPeriod + "," +
+       	   broadPeriod + "," +
+       	   dtaPeriod + "," +
+       	   driver + "," +
+       	   expansionFactor
+       	   );
+       	writer.print(record);
+    }
+        
+    /**
+     * Write a header record
+     * 
+     * @param writer
+     */
+    /**
+     * Write a header record
+     * 
+     * @param writer
+     */
+    public void writeHeader(PrintWriter writer){
+    	String header = "hh_id,person_id,tour_id,trip_id,originTaz,destinationTaz,originMGRA,destinationMGRA,originNode,destinationNode,vehicleType,vehicleOccupancy,tollEligibility,marketSegment,detailedPeriod,broadPeriod,dtaPeriod,driver,expansionFactor";
+    	writer.print(header);
+    }
+        
+}
---
+++ src/main/java/org/sandag/abm/dta/postprocessing/todDisaggregationModel.java
@@ -0,0 +1,117 @@
+package org.sandag.abm.dta.postprocessing;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+import com.pb.common.datafile.TableDataSet;
+import com.pb.common.math.MersenneTwister;
+import org.sandag.abm.ctramp.Util;
+
+public class todDisaggregationModel {
+
+	private static final String PROPERTIES_RANDOMSEED             = "dta.postprocessing.RandomSeed";
+	
+    private HashMap<String,String> rbMap;
+	private MersenneTwister random;
+	private long randomSeed;
+	
+		
+	public PrintWriter tripWriter;
+
+
+    private transient Logger           logger                         = Logger.getLogger("postprocessModel");
+
+	
+	/**
+	 * Default constructor.
+	 */
+	public todDisaggregationModel(HashMap<String,String> rbMap){
+		this.rbMap = rbMap;
+		randomSeed = Util.getIntegerValueFromPropertyMap(rbMap, PROPERTIES_RANDOMSEED);
+		random = new MersenneTwister();
+		random.setSeed(randomSeed);
+ 	}
+			
+	/**
+	 * Read the probability by tod data file, return an array of probabilities.
+	 */
+	public double[] getTODProbabilities(TableDataSet TODData, int numPeriods, String marketSegment){
+	 	
+		// read the tod factors file
+		double [] probabilities;
+		probabilities = new double [numPeriods];
+					
+		//fill in probabilities array
+		for(int i = 0;i<numPeriods;++i){
+			if (marketSegment==null){
+				double prob = (double) TODData.getValueAt(i+1, "Factors");
+				probabilities[i] = prob;
+			}else{
+				double prob = (double) TODData.getValueAt(i+1, marketSegment);
+				probabilities[i] = prob;
+			}
+			
+								
+		}
+		//return array of probabilities
+		return probabilities;
+	}
+	
+	/**
+	 * Read the probability by tod data file, return an array of aggregate TOD values.
+	 */
+	public int[] getTODMap(TableDataSet TODData, int numPeriods){
+	 	
+		// read the tod factors file
+		int [] TODMap;
+		TODMap = new int [numPeriods];
+					
+		//fill in array of more aggregate time period values
+		for(int i = 0;i<numPeriods;++i){
+			int tod = (int) TODData.getValueAt(i+1, "TOD");
+			TODMap[i] = tod;					
+		}
+		//return array of aggregate time period values
+		return TODMap;
+	}
+	
+	/**
+	 * Return a disaggregate time period from an aggregate time period
+	 */
+	public int calculateDisaggregateTOD(int period, int[] TODMap, double[] probabilities, boolean debug){
+		int dtaPeriod = -1;
+		double probability = 0;
+		double cumProbability = 0;
+		double rn = random.nextDouble();
+		int startLoc = 0;
+		
+		if (period>1)
+			startLoc = (period-1)*6 + 18;	
+				
+        // loop through the array of probabilities
+		for (int i=startLoc;i<probabilities.length;++i){
+			
+			// check if the time period for that probability is in the period to be disaggregated
+			if (TODMap[i]!=period)
+				continue;
+			probability = probabilities[i];
+			cumProbability += probability;
+			if(debug){
+				logger.info(period
+						+"     "+(i+1)
+						+"     "+probability
+						+"     "+cumProbability
+						+"     "+rn);
+			}
+			// choose the dta period if the cumulative probability has exceeded the random number
+			if (rn<cumProbability){
+				dtaPeriod = i+1;
+				break;
+			}
+		}
+		return dtaPeriod;
+	}
+    
+}
