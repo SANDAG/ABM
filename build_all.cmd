d3340a6396aa488a846107e6e432738d89eba6a5
wsu <wsu@SDG77222.sandag.org>
2015-03-26 13:46:51 -0700

    (ABM-617) reconfig clusters

--- build_all.cmd
+++ build_all.cmd
@@ -19,14 +19,14 @@ call mvn package -P charger_at,2035 -DskipTests
 call mvn package -P charger_at,2040 -DskipTests
 call mvn package -P charger_at,2050 -DskipTests
 
-call mvn package -P mustang_at,2010 -DskipTests
-call mvn package -P mustang_at,2012 -DskipTests
-call mvn package -P mustang_at,2015 -DskipTests
-call mvn package -P mustang_at,2020 -DskipTests
-call mvn package -P mustang_at,2025 -DskipTests
-call mvn package -P mustang_at,2035 -DskipTests
-call mvn package -P mustang_at,2040 -DskipTests
-call mvn package -P mustang_at,2050 -DskipTests
+call mvn package -P wildcat_at,2010 -DskipTests
+call mvn package -P wildcat_at,2012 -DskipTests
+call mvn package -P wildcat_at,2015 -DskipTests
+call mvn package -P wildcat_at,2020 -DskipTests
+call mvn package -P wildcat_at,2025 -DskipTests
+call mvn package -P wildcat_at,2035 -DskipTests
+call mvn package -P wildcat_at,2040 -DskipTests
+call mvn package -P wildcat_at,2050 -DskipTests
 
 call mvn package -P gaucho_at,2010 -DskipTests
 call mvn package -P gaucho_at,2012 -DskipTests
