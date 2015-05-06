aa1c244ca062fd88c690e4fef8d305a49ddf3c1f
maslovama <maslovama@pbworld.com>
2015-05-06 15:05:45 -0400

    DTA-199 Updates to the GIS Import scripts

--- src/main/aimsun/02a - Create TOD lanes.py
+++ src/main/aimsun/02a - Create TOD lanes.py
@@ -14,20 +14,35 @@ for section in model.getCatalog().getObjectsByType(sectionType).itervalues():
 	origin = section.getOrigin()
 	if origin != None and str(origin.getExternalId()) > "":
 		if section.getDataValueInt(aNodeAtt) == int(str(origin.getExternalId())):
-			maxlanes = max(section.getDataValueInt(ablnoAtt), section.getDataValueInt(ablnaAtt), section.getDataValueInt(ablnpAtt))
+			lna = section.getDataValueInt(ablnaAtt)
+			lno = section.getDataValueInt(ablnoAtt)
+			lnp = section.getDataValueInt(ablnpAtt)
 		else:
-			maxlanes = max(section.getDataValueInt(balnoAtt), section.getDataValueInt(balnaAtt), section.getDataValueInt(balnpAtt))
+			lna = section.getDataValueInt(balnaAtt)
+			lno = section.getDataValueInt(balnoAtt)
+			lnp = section.getDataValueInt(balnpAtt)
 	else:
 		destination = section.getDestination()
 		if destination != None and str(destination.getExternalId()) > "":
 			if section.getDataValueInt(aNodeAtt) == int(str(destination.getExternalId())):
-				maxlanes = max(section.getDataValueInt(balnoAtt), section.getDataValueInt(balnaAtt), section.getDataValueInt(balnpAtt))
+				lna = section.getDataValueInt(balnaAtt)
+				lno = section.getDataValueInt(balnoAtt)
+				lnp = section.getDataValueInt(balnpAtt)
 			else:
-				maxlanes = max(section.getDataValueInt(ablnoAtt), section.getDataValueInt(ablnaAtt), section.getDataValueInt(ablnpAtt))
-	if maxlanes > section.getNbFullLanes():
+				lna = section.getDataValueInt(ablnaAtt)
+				lno = section.getDataValueInt(ablnoAtt)
+				lnp = section.getDataValueInt(ablnpAtt)
+	if lna == 9:
+		lna = 0
+	if lno == 9:
+		lno = 0
+	if lnp == 9:
+		lnp = 0
+	maxlanes = max(lna, lno, lnp)
+	if maxlanes != section.getNbFullLanes():
 		#print "Section %i (%s): lanes %i, max %i" % (section.getId(), section.getExternalId(), section.getNbFullLanes(), maxlanes)
 		cmd = GKSectionChangeNbLanesCmd()
-		cmd.setData(section, len(section.getLanes())+(maxlanes - section.getNbFullLanes()))
+		cmd.setData(section, maxlanes)
 		model.getCommander().addCommand(cmd)
 
 print "TOD lanes created!"
