1797ede66bb95a5b8bbb314200c95b08047e6845
wsu <wsu@SDG77222.sandag.org>
2015-09-08 11:01:26 -0700

    (ABM-662) Add 4th node to cluster

--- src/main/resources/StartHHAndNodes.cmd
+++ src/main/resources/StartHHAndNodes.cmd
@@ -8,10 +8,13 @@ rem remove active connections so that limit is not exceeded
 net session /delete /Y
 
 call %PATH_NO_DRIVE%\bin\CTRampEnv.bat
-If %SNODE%==true goto :snode
 
+%PATH_NO_DRIVE%\bin\pskill \\%MAIN%  java
 %PATH_NO_DRIVE%\bin\pskill \\%NODE1%  java
 %PATH_NO_DRIVE%\bin\pskill \\%NODE2%  java
+%PATH_NO_DRIVE%\bin\pskill \\%NODE3%  java
+
+If %SNODE%==true goto :snode
 
 rem Start HH Manager on master node
 call %PATH_NO_DRIVE%\bin\runHhMgr.cmd %PROJECT_DRIVE% %PATH_NO_DRIVE%
@@ -23,6 +26,10 @@ start %PATH_NO_DRIVE%\bin\psExec \\%NODE1% -s -c -f %PATH_NO_DRIVE%\bin\%MAPANDR
 rem start remote worker nodes: SANDAG03
 set PROGRAMSTRING=%PATH_NO_DRIVE%\bin\runSandag03.cmd %MAPDRIVE% %PATH_NO_DRIVE%
 start %PATH_NO_DRIVE%\bin\psExec \\%NODE2% -s -c -f %PATH_NO_DRIVE%\bin\%MAPANDRUN% %MAPDRIVE% %MAPDRIVEFOLDER% %PASSWORD% %USERNAME% %PATH_NO_DRIVE% %PROGRAMSTRING%
+
+rem start remote worker nodes: SANDAG04
+set PROGRAMSTRING=%PATH_NO_DRIVE%\bin\runSandag04.cmd %MAPDRIVE% %PATH_NO_DRIVE%
+start %PATH_NO_DRIVE%\bin\psExec \\%NODE3% -s -c -f %PATH_NO_DRIVE%\bin\%MAPANDRUN% %MAPDRIVE% %MAPDRIVEFOLDER% %PASSWORD% %USERNAME% %PATH_NO_DRIVE% %PROGRAMSTRING%
 goto :end
 
 :snode
