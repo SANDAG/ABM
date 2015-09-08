e8690064a26bef20b8d5f1241f149bb8a9872270
wsu <wsu@SDG77222.sandag.org>
2015-09-08 15:30:51 -0700

    (ABM-667) Add TransCAD installation folder to PATH in CreateD2TAccessFile.bat

--- src/main/resources/CreateD2TAccessFile.bat
+++ src/main/resources/CreateD2TAccessFile.bat
@@ -4,5 +4,6 @@ set PROJECT_DIRECTORY=%2
 %PROJECT_DRIVE%
 cd %PROJECT_DIRECTORY%
 call bin\CTRampEnv.bat
+set PATH=%TRANSCAD_PATH%
 
-%JAVA_64_PATH%\bin\java -Xms%MEMORY_DATAEXPORT_MIN% -Xmx%MEMORY_DATAEXPORT_MAX% -cp application/*;conf/;%TRANSCAD_PATH%/GISDK/Matrices/* -Dproject.folder=%PROJECT_DIRECTORY% org.sandag.abm.application.SandagMGRAtoPNR %PROPERTIES_NAME%
+%JAVA_64_PATH%\bin\java -Xms%MEMORY_DATAEXPORT_MIN% -Xmx%MEMORY_DATAEXPORT_MAX% -Dlog4j.configuration=log4j_d2t.xml -cp application/*;conf/;%TRANSCAD_PATH%/GISDK/Matrices/* -Dproject.folder=%PROJECT_DIRECTORY% org.sandag.abm.application.SandagMGRAtoPNR %PROPERTIES_NAME%
