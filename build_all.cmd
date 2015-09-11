66ee9309529a6df1e44e895d75d5144570357839
wsu <wsu@SDG77222.sandag.org>
2015-09-11 14:14:24 -0700

    (ABM-660) single node setting; update build batch

--- build_all.cmd
+++ build_all.cmd
@@ -10,15 +10,6 @@ call mvn package -P aztec,2035 -DskipTests
 call mvn package -P aztec,2040 -DskipTests
 call mvn package -P aztec,2050 -DskipTests
 
-call mvn package -P charger,2010 -DskipTests
-call mvn package -P charger,2012 -DskipTests
-call mvn package -P charger,2015 -DskipTests
-call mvn package -P charger,2020 -DskipTests
-call mvn package -P charger,2025 -DskipTests
-call mvn package -P charger,2035 -DskipTests
-call mvn package -P charger,2040 -DskipTests
-call mvn package -P charger,2050 -DskipTests
-
 call mvn package -P wildcat,2010 -DskipTests
 call mvn package -P wildcat,2012 -DskipTests
 call mvn package -P wildcat,2015 -DskipTests
