b31e784ad1b6d1aaefca55b62c4f9e0a9e551a40
wsu <wsu@SDG77222.sandag.org>
2015-09-11 14:03:18 -0700

    (ABM-660) single node setting; update build batch file

--- build_all.cmd
+++ build_all.cmd
@@ -1,38 +1,47 @@
 e:
 cd \apps\ABM
 
-call mvn package -P aztec_at,2010 -DskipTests
-call mvn package -P aztec_at,2012 -DskipTests
-call mvn package -P aztec_at,2015 -DskipTests
-call mvn package -P aztec_at,2020 -DskipTests
-call mvn package -P aztec_at,2025 -DskipTests
-call mvn package -P aztec_at,2035 -DskipTests
-call mvn package -P aztec_at,2040 -DskipTests
-call mvn package -P aztec_at,2050 -DskipTests
+call mvn package -P aztec,2010 -DskipTests
+call mvn package -P aztec,2012 -DskipTests
+call mvn package -P aztec,2015 -DskipTests
+call mvn package -P aztec,2020 -DskipTests
+call mvn package -P aztec,2025 -DskipTests
+call mvn package -P aztec,2035 -DskipTests
+call mvn package -P aztec,2040 -DskipTests
+call mvn package -P aztec,2050 -DskipTests
 
-call mvn package -P charger_at,2010 -DskipTests
-call mvn package -P charger_at,2012 -DskipTests
-call mvn package -P charger_at,2015 -DskipTests
-call mvn package -P charger_at,2020 -DskipTests
-call mvn package -P charger_at,2025 -DskipTests
-call mvn package -P charger_at,2035 -DskipTests
-call mvn package -P charger_at,2040 -DskipTests
-call mvn package -P charger_at,2050 -DskipTests
+call mvn package -P charger,2010 -DskipTests
+call mvn package -P charger,2012 -DskipTests
+call mvn package -P charger,2015 -DskipTests
+call mvn package -P charger,2020 -DskipTests
+call mvn package -P charger,2025 -DskipTests
+call mvn package -P charger,2035 -DskipTests
+call mvn package -P charger,2040 -DskipTests
+call mvn package -P charger,2050 -DskipTests
 
-call mvn package -P wildcat_at,2010 -DskipTests
-call mvn package -P wildcat_at,2012 -DskipTests
-call mvn package -P wildcat_at,2015 -DskipTests
-call mvn package -P wildcat_at,2020 -DskipTests
-call mvn package -P wildcat_at,2025 -DskipTests
-call mvn package -P wildcat_at,2035 -DskipTests
-call mvn package -P wildcat_at,2040 -DskipTests
-call mvn package -P wildcat_at,2050 -DskipTests
+call mvn package -P wildcat,2010 -DskipTests
+call mvn package -P wildcat,2012 -DskipTests
+call mvn package -P wildcat,2015 -DskipTests
+call mvn package -P wildcat,2020 -DskipTests
+call mvn package -P wildcat,2025 -DskipTests
+call mvn package -P wildcat,2035 -DskipTests
+call mvn package -P wildcat,2040 -DskipTests
+call mvn package -P wildcat,2050 -DskipTests
 
-call mvn package -P gaucho_at,2010 -DskipTests
-call mvn package -P gaucho_at,2012 -DskipTests
-call mvn package -P gaucho_at,2015 -DskipTests
-call mvn package -P gaucho_at,2020 -DskipTests
-call mvn package -P gaucho_at,2025 -DskipTests
-call mvn package -P gaucho_at,2035 -DskipTests
-call mvn package -P gaucho_at,2040 -DskipTests
-call mvn package -P gaucho_at,2050 -DskipTests
+call mvn package -P gaucho,2010 -DskipTests
+call mvn package -P gaucho,2012 -DskipTests
+call mvn package -P gaucho,2015 -DskipTests
+call mvn package -P gaucho,2020 -DskipTests
+call mvn package -P gaucho,2025 -DskipTests
+call mvn package -P gaucho,2035 -DskipTests
+call mvn package -P gaucho,2040 -DskipTests
+call mvn package -P gaucho,2050 -DskipTests
+
+call mvn package -P local,2010 -DskipTests
+call mvn package -P local,2012 -DskipTests
+call mvn package -P local,2015 -DskipTests
+call mvn package -P local,2020 -DskipTests
+call mvn package -P local,2025 -DskipTests
+call mvn package -P local,2035 -DskipTests
+call mvn package -P local,2040 -DskipTests
+call mvn package -P local,2050 -DskipTests
