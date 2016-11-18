5601f49975eb552ee5d6763038c4542a5e8d2916
wsu <wsu@SDG77222.sandag.org>
2016-11-18 11:39:22 -0800

    (ABM-812) Customize, modify, and re-organize validation procedure; include validation procedure in build

--- src/main/python/pythonGUI/ABMGUI.py
+++ src/main/python/pythonGUI/ABMGUI.py
@@ -1,5 +1,5 @@
 __author__ = 'wsu'
-#Wu.Sun@sandag.org 10-27-2016
+#Wu.Sun@sandag.org 11-18-2016
 import Tkinter
 import Tkconstants
 import tkFileDialog
@@ -22,6 +22,7 @@ class CreateScenarioGUI(Tkinter.Frame):
             self.defaultScenarioDir="T:\\projects\\sr13"
             self.defaultNetworkDir="T:\\projects\\sr13\\version13_3_0\\network_build"
 
+            #study space section
             self.buttonVar= IntVar(root)
             self.yButton=Radiobutton(body, text="Yes", variable=self.buttonVar, value=1, command=self.initStudy)
             self.nButton=Radiobutton(body, text="No", variable=self.buttonVar, value=0,command=self.initStudy)
@@ -43,18 +44,19 @@ class CreateScenarioGUI(Tkinter.Frame):
             self.studynetworkpath.grid(row=4, column=1, sticky=sticky)
             self.studynetworkpath.delete(0, Tkconstants.END)
             self.studynetworkpath.insert(0, self.defaultNetworkDir)
-            self.studynetworkbutton = Tkinter.Button(body, text=u"...",width=4,command=lambda: self.get_path("studynetwork"))
+            self.studynetworkbutton = Tkinter.Button(body, text=u"...",width=4,command=lambda:self.get_path("studynetwork"))
             self.studynetworkbutton.grid(row=4, column=2)
 
-            self.copyButton = Tkinter.Button(body, text=u"Create", font=("Helvetica", 8, 'bold'),width=10, command=lambda: self.checkPath("study"))
+            self.copyButton = Tkinter.Button(body, text=u"Create", font=("Helvetica", 8, 'bold'),width=10, command=lambda:self.checkPath("study"))
             self.copyButton.grid(row=5,column=0,columnspan=4)
 
+            #scenario section
             Tkinter.Label(body, text=divider, font=("Helvetica", 11, 'bold'), width=50, fg='royal blue').grid(row=6,columnspan=5)
             Tkinter.Label(body, text=u"Create an ABM scenario", font=("Helvetica", 10, 'bold')).grid(row=7,columnspan=3)
 
             Tkinter.Label(body, text=u"Version", font=("Helvetica", 8, 'bold')).grid(row=8)
             var = StringVar(root)
-            self.version="version_13_3_1_SNAPSHOT"
+            self.version="version_13_3_1_SNAPSHOT_test"
             optionList=["version_13_3_0", "version_13_3_1_SNAPSHOT"]
             option=Tkinter.OptionMenu(body,var,*optionList,command=self.setversion)
             option.config(width=50)
@@ -71,24 +73,47 @@ class CreateScenarioGUI(Tkinter.Frame):
             Tkinter.Label(body, text=u"Scenario Folder", font=("Helvetica", 8, 'bold')).grid(row=10)
             self.scenariopath = Tkinter.Entry(body, width=40)
             self.scenariopath.grid(row=10, column=1, sticky=sticky)
-            button = Tkinter.Button(body, text=u"...",width=4,command=lambda: self.get_path("scenario"))
+            button = Tkinter.Button(body, text=u"...",width=4,command=lambda:self.get_path("scenario"))
             button.grid(row=10, column=2)
 
             Tkinter.Label(body, text=u"Network Folder",font=("Helvetica", 8, 'bold')).grid(row=11)
             self.networkpath = Tkinter.Entry(body, width=40)
             self.networkpath.grid(row=11, column=1, sticky=sticky)
-            button = Tkinter.Button(body, text=u"...",width=4,command=lambda: self.get_path("network"))
+            button = Tkinter.Button(body, text=u"...",width=4,command=lambda:self.get_path("network"))
             button.grid(row=11, column=2)
 
-            buttons = Tkinter.Frame(self)
-            buttons.pack()
-            botton = Tkinter.Button(buttons, text=u"Create", font=("Helvetica", 8, 'bold'),width=10, command=lambda: self.checkPath("scenario"))
-            botton.pack(side=Tkconstants.LEFT)
-            Tkinter.Frame(buttons, width=10).pack(side=Tkconstants.LEFT)
-            button = Tkinter.Button(buttons, text=u"Quit", font=("Helvetica", 8, 'bold'), width=10, command=self.quit)
-            button.pack(side=Tkconstants.RIGHT)
+            self.scenarioButton = Tkinter.Button(body, text=u"Create", font=("Helvetica", 8, 'bold'),width=10, command=lambda:self.checkPath("scenario"))
+            self.scenarioButton.grid(row=12,column=0,columnspan=4)
+
+            #validation section
+            Tkinter.Label(body, text=divider, font=("Helvetica", 11, 'bold'), width=50, fg='royal blue').grid(row=13,columnspan=5)
+            Tkinter.Label(body, text=u"Validate a Scenario", font=("Helvetica", 10, 'bold')).grid(row=14,columnspan=3)
+            self.vbuttonVar= IntVar(root)
+            self.vyButton=Radiobutton(body, text="Yes", variable=self.vbuttonVar, value=1, command=self.initValidation)
+            self.vnButton=Radiobutton(body, text="No", variable=self.vbuttonVar, value=0,command=self.initValidation)
+            self.vyButton.grid(row=15,column=0, columnspan=2)
+            self.vnButton.grid(row=15,column=1, columnspan=2)
+
+            Tkinter.Label(body, text=u"Scenario Number", font=("Helvetica", 8, 'bold')).grid(row=16)
+            vv = StringVar(root)
+            vv.set("540")
+            self.validationScenario="540"
+            vv.trace("w", lambda name, index, mode, vv=vv: self.setValidationScenario(vv))
+            self.ve = Entry(body, textvariable=vv)
+            self.ve.config(width=15)
+            self.ve.grid(row=16,column=1,sticky=sticky)
+
+            Tkinter.Label(body, text=u"Output Folder", font=("Helvetica", 8, 'bold')).grid(row=17)
+            self.validationpath = Tkinter.Entry(body, width=40)
+            self.validationpath.grid(row=17, column=1, sticky=sticky)
+            self.voutbutton = Tkinter.Button(body, text=u"...",width=4,command=lambda:self.get_path("validate"))
+            self.voutbutton.grid(row=17, column=2)
+
+            self.validateButton = Tkinter.Button(body, text=u"Validate", font=("Helvetica", 8, 'bold'),width=10, command=lambda:self.checkPath("validate"))
+            self.validateButton.grid(row=18,column=0,columnspan=4)
 
             self.initStudy()
+            self.initValidation()
 
         def initStudy(self):
             #disable study setting buttons
@@ -106,6 +131,20 @@ class CreateScenarioGUI(Tkinter.Frame):
                 self.studynetworkbutton.config(state=DISABLED)
                 self.copyButton.configure(state=DISABLED)
 
+        def initValidation(self):
+            #disable study setting buttons
+            if self.vbuttonVar.get()==1:
+                self.validationpath.config(state=NORMAL)
+                self.validateButton.configure(state=NORMAL)
+                self.voutbutton.configure(state=NORMAL)
+                self.ve.configure(state=NORMAL)
+            #enable study setting buttons
+            elif self.vbuttonVar.get()==0:
+                self.validationpath.config(state=DISABLED)
+                self.validateButton.configure(state=DISABLED)
+                self.voutbutton.configure(state=DISABLED)
+                self.ve.configure(state=DISABLED)
+
         #set default input and network paths based on selected year
         def setversion(self,value):
             self.version=value
@@ -119,11 +158,6 @@ class CreateScenarioGUI(Tkinter.Frame):
             self.networkpath.delete(0, Tkconstants.END)
             self.networkpath.insert(0, self.defaultpath)
             self.year=value
-            return
-
-        #set cluster
-        def setcluster(self,value):
-            self.cluster=value
             return
 
         #set default options for folded browsers
@@ -143,6 +177,11 @@ class CreateScenarioGUI(Tkinter.Frame):
                     spath = os.path.normpath(path)
                     self.scenariopath.delete(0, Tkconstants.END)
                     self.scenariopath.insert(0, spath)
+            elif type=="validate":
+                if path:
+                    vpath = os.path.normpath(path)
+                    self.validationpath.delete(0, Tkconstants.END)
+                    self.validationpath.insert(0, vpath)
             elif type=="network":
                 if path:
                     npath = os.path.normpath(path)
@@ -189,22 +228,38 @@ class CreateScenarioGUI(Tkinter.Frame):
                         popupMsg.popupmsg(self,"Network folder is empty!",1,type)
                     else:
                         self.executeBatch(type)
+            elif type=="validate":
+                if not self.validationScenario:
+                    popupMsg.popupmsg(self,"No validation scenario was selected!",1,type)
+                else:
+                    if not self.validationpath.get():
+                        popupMsg.popupmsg(self,"Validation output directory is empty!",1,type)
+                    else:
+                        self.executeBatch(type)
             return
 
         #execute DOS commands
         def executeBatch(self, type):
             self.popup.destroy()
+            msg="You have successfully created the "+ type+"!"
+            dir=self.releaseDir+"\\"+self.version
             if type=="scenario":
                 commandstr=u"create_scenario.cmd "+self.scenariopath.get()+" "+self.year+" "+self.networkpath.get()
             elif type=="study":
                 commandstr=u"copy_networkfiles_to_study.cmd "+self.studypath.get()+" "+self.studynetworkpath.get()
+            elif type=="validate":
+                commandstr=u"validate.cmd "+self.validationScenario+" "+self.validationpath.get()+"\\"
+                msg="Validation resulta are in "+self.validationpath.get()
+                dir=self.releaseDir+"\\"+self.version+'\\validation\\'
             print commandstr
-            os.chdir(self.releaseDir+"\\"+self.version+'\\')
+            os.chdir(dir)
             os.system(commandstr)
             self.popup=Tkinter.Tk()
-            msg="You have successfully created the "+ type+"!"
             popupMsg.popupmsg(self,msg,1,type)
             return
+
+        def setValidationScenario(self,value):
+            self.validationScenario=value.get()
 
 root = Tkinter.Tk()
 root.resizable(True, False)
