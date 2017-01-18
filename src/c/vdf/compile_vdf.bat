b3f261a785adad15aeb83c4d18f95ef7a76d1ec2
joel.freedman <joel.freedman@rsginc.com>
2017-01-17 18:01:41 -0800

    Fixed VDF bug (ABM-816)

--- src/c/vdf/compile_vdf.bat
+++ src/c/vdf/compile_vdf.bat
@@ -5,7 +5,7 @@ set PATH=%PATH%;%vs_path%\vc;%vs_path%\bin
 call vcvarsall.bat x64
 
 
-rem CL /nologo /Gr /LD /O2 /Fe../../shrp.vdf SHRP.c user32.lib
+rem CL /nologo /Gr /LD /O2 /Feshrp.rev.vdf SHRP.rev.c user32.lib
 rem for debug use: 
-CL /nologo /Gr /LD /Zi /Od /Fe../../shrp.vdf SHRP.c user32.lib
+CL /nologo /Gr /LD /Zi /Od /Feshrp.rev.vdf SHRP.rev.c user32.lib
 set PATH=%OLDPATH%
