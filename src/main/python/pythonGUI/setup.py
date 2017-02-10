86c7874b463f8fd814fb2fac03602860a73f6555
wsu <wsu@SDG77222.sandag.org>
2017-02-10 15:56:04 -0800

    (ABM-819) release 13.3.1

--- src/main/python/pythonGUI/setup.py
+++ src/main/python/pythonGUI/setup.py
@@ -2,7 +2,7 @@ __author__ = 'wsu'
 from distutils.core import setup
 import py2exe
 
-setup(windows=['./src/main/python/pythonGUI/createScenario.py'])
-setup(windows=['./src/main/python/pythonGUI/ABMGUI.py'])
 setup(windows=['./src/main/python/pythonGUI/parameterEditor.py'])
+setup(windows=['./src/main/python/pythonGUI/createStudyAndScenario.py'])
+setup(windows=['./src/main/python/pythonGUI/validatorGUI.py'])
 
