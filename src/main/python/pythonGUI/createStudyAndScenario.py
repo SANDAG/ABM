af7eb45dcb68e7bed9468d5788b5807dd0c7dc5f
wsu <wsu@SDG77222.sandag.org>
2017-06-08 14:06:43 -0700

    (ABM-999) Add 2015 to release 13.3.2

--- src/main/python/pythonGUI/createStudyAndScenario.py
+++ src/main/python/pythonGUI/createStudyAndScenario.py
@@ -64,7 +64,7 @@ class CreateScenarioGUI(Tkinter.Frame):
             Tkinter.Label(body, text=u"Year", font=("Helvetica", 8, 'bold')).grid(row=9)
             var = StringVar(root)
             self.year="2012"
-            optionList=["2012", "2014", "2016", "2017", "2020", "2025", "2030", "2035", "2040", "2045", "2050"]
+            optionList=["2012", "2014", "2015", "2016", "2017", "2020", "2025", "2030", "2035", "2040", "2045", "2050"]
             option=Tkinter.OptionMenu(body,var,*optionList,command=self.setyear)
             option.config(width=50)
             option.grid(row=9, column=1)
