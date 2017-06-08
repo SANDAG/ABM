4a224e45d1141b4f85c504a752adcde98c7f33ea
wsu <wsu@SDG77222.sandag.org>
2017-06-08 14:17:36 -0700

    (ABM-999) Remove 13.3.1 from release list in GUI

--- src/main/python/pythonGUI/createStudyAndScenario.py
+++ src/main/python/pythonGUI/createStudyAndScenario.py
@@ -56,7 +56,7 @@ class CreateScenarioGUI(Tkinter.Frame):
             Tkinter.Label(body, text=u"Version", font=("Helvetica", 8, 'bold')).grid(row=8)
             var = StringVar(root)
             self.version="version_13_3_2"
-            optionList=["version_13_3_0", "version_13_3_1", "version_13_3_2"]
+            optionList=["version_13_3_0", "version_13_3_2"]
             option=Tkinter.OptionMenu(body,var,*optionList,command=self.setversion)
             option.config(width=50)
             option.grid(row=8, column=1)
