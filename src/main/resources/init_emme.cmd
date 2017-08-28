a39fe4f49f08b7e4a7368a7e38c5c61a1ec255d5
wsu <wsu@SDG77222.sandag.org>
2017-08-28 15:49:06 -0700

    (ABM-880) Add init_emme in build process

--- src/main/resources/init_emme.cmd
+++ src/main/resources/init_emme.cmd
@@ -7,12 +7,10 @@ rem add EMME to PATH
 set PATH=%EMMEPATH%\\programs;%EMMEPATH%\\python27;%PATH%
 
 rem create EMME project folder
-python .\\common\\python\\emme\\init_emme_project.py -r %SCENARIO_FOLDER% -t testbank 
-rem python ./src/main/emme/init_emme_project.py -r ${project.build.directory}/${project.name}/${project.version} -t testbank 
+python .\\common\\python\\emme\\init_emme_project.py -r %SCENARIO_FOLDER% -t emmebank 
 
 rem create toolbox
 python .\\common\\python\\emme\\toolbox\\build_toolbox.py -s %SCENARIO_FOLDER%\emme_project\Scripts -p %SCENARIO_FOLDER%\emme_project\Scripts\sandag_toolbox.mtbx
-rem python ./src/main/emme/toolbox/build_toolbox.py -s ${project.build.directory}/${project.name}/${project.version}\emme_project\Scripts -p ${project.build.directory}/${project.name}/${project.version}\emme_project\Scripts\sandag_toolbox.mtbx
 
 :usage
 @echo Usage: %0 ^<scenario_folder^>
