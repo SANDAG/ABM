a340b91d86a449521e88aa6e115c0de801ba0398
wsu <wsu@SDG77222.sandag.org>
2017-08-28 16:19:51 -0700

    (ABM-883) Add YAML to build

--- src/main/resources/init_emme.cmd
+++ src/main/resources/init_emme.cmd
@@ -12,5 +12,7 @@ python .\\common\\python\\emme\\init_emme_project.py -r %SCENARIO_FOLDER% -t emm
 rem create toolbox
 python .\\common\\python\\emme\\toolbox\\build_toolbox.py -s %SCENARIO_FOLDER%\emme_project\Scripts -p %SCENARIO_FOLDER%\emme_project\Scripts\sandag_toolbox.mtbx
 
+copy .\\common\\python\\emme\\yaml\\*.* %SCENARIO_FOLDER%\emme_project\Scripts\yaml
+
 :usage
 @echo Usage: %0 ^<scenario_folder^>
