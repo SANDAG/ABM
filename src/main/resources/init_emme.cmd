bd00db338e6046ab4ba926ddd2eb1f7f026703e2
wsu <wsu@SDG77222.sandag.org>
2017-08-29 08:39:48 -0700

    (ABM-880) add init_emme to build; correct build toolbox path error

--- src/main/resources/init_emme.cmd
+++ src/main/resources/init_emme.cmd
@@ -10,7 +10,9 @@ rem create EMME project folder
 python .\\common\\python\\emme\\init_emme_project.py -r %SCENARIO_FOLDER% -t emmebank 
 
 rem create toolbox
-python .\\common\\python\\emme\\toolbox\\build_toolbox.py -s %SCENARIO_FOLDER%\emme_project\Scripts -p %SCENARIO_FOLDER%\emme_project\Scripts\sandag_toolbox.mtbx
+python .\\common\\python\\emme\\toolbox\\build_toolbox.py -s .\\common\\python\\emme\\toolbox -p %SCENARIO_FOLDER%\emme_project\Scripts\sandag_toolbox.mtbx
+
+mkdir %SCENARIO_FOLDER%\emme_project\Scripts\yaml
 
 copy .\\common\\python\\emme\\yaml\\*.* %SCENARIO_FOLDER%\emme_project\Scripts\yaml
 
