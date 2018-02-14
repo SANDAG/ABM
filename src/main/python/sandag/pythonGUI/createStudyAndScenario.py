390ef147ee1468dcf625e5107c8013b67505333d
wsu <wsu@SDG77222.sandag.org>
2018-02-14 11:23:00 -0800

    (ABM-925) Add 2016 as a standard year in GUI.

--- src/main/python/sandag/pythonGUI/createStudyAndScenario.py
+++ src/main/python/sandag/pythonGUI/createStudyAndScenario.py
@@ -66,9 +66,9 @@ class CreateScenarioGUI(Tkinter.Frame):
             var = StringVar(root)
             self.year="2012"
             # standard release years
-            #optionList=["2012", "2014", "2020", "2025", "2030", "2035", "2040", "2045", "2050"]
+            optionList=["2012", "2014", "2016","2020", "2025", "2030", "2035", "2040", "2045", "2050"]
             # development/testing years; un-comment if build testing years
-            optionList=["2015", "2016", "2017"]
+            #optionList=["2015", "2016", "2017"]
             option=Tkinter.OptionMenu(body,var,*optionList,command=self.setyear)
             option.config(width=50)
             option.grid(row=9, column=1)
