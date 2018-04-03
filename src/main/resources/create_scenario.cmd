874acd9e091cfa8d2d9f90b03125dfdcf2a71be8
wsu <wsu@SDG77222.sandag.org>
2018-04-02 17:01:22 -0700

    (ABM-963) Reorganize common resources

--- src/main/resources/create_scenario.cmd
+++ src/main/resources/create_scenario.cmd
@@ -13,56 +13,38 @@ set FOLDERS=input application bin conf input_truck logFiles output python report
 for %%i in (%FOLDERS%) do (
 md %SCENARIO_FOLDER%\%%i)
 
-#grant full permissions to scenario folder
+rem grant full permissions to scenario folder
 cacls %SCENARIO_FOLDER% /t /e /g Everyone:f
 
-#copy master server-config.csv to a scenario folder
+rem copy master server-config.csv to a scenario folder
 xcopy /Y T:\ABM\release\ABM\config\server-config.csv %SCENARIO_FOLDER%\conf
 
+rem setup model folders
 xcopy /Y .\common\application\"*.*" %SCENARIO_FOLDER%\application
-xcopy /Y .\common\input_truck\"*.*" %SCENARIO_FOLDER%\input_truck
-
 xcopy /Y/E .\common\python\"*.*" %SCENARIO_FOLDER%\python
 xcopy /Y/E .\common\sql\"*.*" %SCENARIO_FOLDER%\sql
 xcopy /Y .\common\uec\"*.*" %SCENARIO_FOLDER%\uec
 xcopy /Y .\common\bin\"*.*" %SCENARIO_FOLDER%\bin
 xcopy /Y .\conf\%YEAR%\"*.*" %SCENARIO_FOLDER%\conf
-#xcopy /Y .\"*.txt" %SCENARIO_FOLDER%
 xcopy /Y .\common\output\"*.*" %SCENARIO_FOLDER%\output
 
 @echo copy year specific folders
 del %SCENARIO_FOLDER%\input /q
-xcopy /Y .\input\%YEAR%\"*.*" %SCENARIO_FOLDER%\input
-#copy master tazcentroids_cvm.csv to a scenario input folder
-xcopy /Y T:\ABM\release\ABM\config\tazcentroids_cvm.csv %SCENARIO_FOLDER%\input
-#copy cvm paramter tables to input folder
-xcopy /Y .\common\input_cvm\"*.*" %SCENARIO_FOLDER%\input
-
-@echo replace \ with \\
-set SCENARIO_FOLDER2=%SCENARIO_FOLDER:\=\\%
-
-@echo copy network inputs
+rem copy network inputs
 call copy_networks.cmd %NETWORKDIR% %SCENARIO_FOLDER%\input
+rem copy other input files
+xcopy /Y .\input\%YEAR%\"*.*" %SCENARIO_FOLDER%\input
+rem copy common geography files to input folder
+xcopy /Y .\common\input\geography\"*.*" %SCENARIO_FOLDER%\input
+rem copy ctm paramter tables to input folder
+xcopy /Y .\common\input\ctm\"*.*" %SCENARIO_FOLDER%\input
+rem copy common model files to input folder
+xcopy /Y .\common\input\model\"*.*" %SCENARIO_FOLDER%\input
+rem copy common truck files to input_truck folder
+xcopy /Y .\common\input\truck\"*.*" %SCENARIO_FOLDER%\input_truck
 
 @echo init emme folder
 call init_emme.cmd %SCENARIO_FOLDER%
-
-rem @echo off & setlocal
-rem set t=%SCENARIO_FOLDER%
-rem for /f "tokens=1,2 delims=:" %%a in ("%t%") do (
-rem    set x=%%a
-rem    set y=%%b
-rem    )
-
-rem @echo check AT and Transit networks consistency
-
-rem @echo call %SCENARIO_FOLDER%\bin\checkAtTransitNetworkConsistency.cmd %x%: %y%
-rem call %SCENARIO_FOLDER%\bin\checkAtTransitNetworkConsistency.cmd %x%: %y%
-rem @echo -----------------------------------------------------------------
-rem @echo If error message logged out in DOS window, CHECK your SANDAG_Bike_Node.dbf and tapcov.dbf.
-rem @echo If you used default inputs, check in release/input;
-rem @echo otherwise, check in the network folder you provided %NETWORKDIR%.
-rem @echo -----------------------------------------------------------------
 
 :usage
 
