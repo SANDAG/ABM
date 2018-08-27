5816311fe2cf57a9b2e35bc77ac486df44da5088
wsu <wsu@SDG77222.sandag.org>
2018-08-27 14:00:29 -0700

    (ABM-1086) Add an option to create shadow pricing from scratch.

--- src/main/resources/updateShadowPricing.bat
+++ src/main/resources/updateShadowPricing.bat
@@ -8,6 +8,7 @@ set ITERATIONS=%5
 set "SCEN_DIR=%PROJECT_DRIVE%%PROJECT_DIRECTORY%"
 set "SCEN_DIR_FWD=%PROJECT_DRIVE%%PROJECT_DIRECTORY_FWD%"
 
+
 python.exe %SCEN_DIR%\python\sandag\shadowpricing\scaleShadowPricing.py  %SCEN_DIR_FWD%\output %ITERATIONS% work %SAMPLE_RATE%
 python.exe %SCEN_DIR%\python\sandag\shadowpricing\scaleShadowPricing.py  %SCEN_DIR_FWD%\output %ITERATIONS% school %SAMPLE_RATE%
 
