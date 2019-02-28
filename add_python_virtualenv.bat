4fe38c8e607aa78f0bd79cb925403731b96cc13e
inrokevin <kevin@inrsoftware.com>
2019-02-28 14:24:27 -0500

    Adding a bat script to generate a virtual environment for custom Python install and the list of libraries in requirements.txt

---
+++ add_python_virtualenv.bat
@@ -0,0 +1,16 @@
+REM
+REM Must be run from the Emme Shell for the installed version of Emme
+REM Requires administrative permssions
+REM Requires the "requirements.txt" with the list of Python libraries
+REM and versions on the C drive (or change the path to requirements.txt)
+REM
+python -m pip install --upgrade pip
+python -m pip install virtualenv
+cd C:\
+IF NOT EXIST python_virtualenv (
+    mkdir python_virtualenv
+)
+python -m virtualenv python_virtualenv\abm14_1
+python_virtualenv\abm_14_1\Scripts\activate
+pip install -r requirements.txt
+deactivate
