743bddb758f104425404fcecea2dc0093ddebd86
Cherry Liu <Cherry.Liu@sandag.org>
2019-11-14 15:28:31 -0800

    Add setup.py back to remote repo

---
+++ src/main/python/pythonGUI/setup.py
@@ -0,0 +1,9 @@
+__author__ = 'wsu'
+from distutils.core import setup
+import py2exe
+
+setup(windows=['./src/main/python/pythonGUI/parameterEditor.py'])
+setup(windows=['./src/main/python/pythonGUI/createStudyAndScenario.py'])
+setup(windows=['./src/main/python/pythonGUI/validatorGUI.py'])
+
+
