f129e6ba27098ca921f02d9b29a42c36e02b6f06
Cundo Arellano <cundo.a@gmail.com>
2020-08-18 15:47:50 -0700

    input checker:  - added checks inputs_checks.csv  - added inputs to inputs_list.csv  - modified input checker script to allow for input subset  - modified output summary file structure

--- src/main/emme/toolbox/import/input_checker.py
+++ src/main/emme/toolbox/import/input_checker.py
@@ -34,6 +34,7 @@ from simpledbf import Dbf5
 import inro.modeller as _m
 import inro.emme.database.emmebank as _eb
 import inro.director.util.qtdialog as dialog
+import textwrap
 
 warnings.filterwarnings("ignore")
 
@@ -59,6 +60,7 @@ class input_checker(_m.Tool()):
 		self.inputs_list_path = ''
 		self.inputs_checks_path = ''
 		self.log_path = ''
+		self.logical_log_path = ''
 		self.prop_input_paths = {}
 		self.inputs_list = pd.DataFrame()
 		self.inputs_checks = pd.DataFrame()
@@ -137,6 +139,9 @@ class input_checker(_m.Tool()):
 		_m.logbook_write("Conducting checks...")
 		self.checks()
 
+		_m.logbook_write("Writing logical fail logs...")
+		self.write_logical_log()
+
 		_m.logbook_write("Writing logs...")
 		self.write_log()
 
@@ -165,20 +170,20 @@ class input_checker(_m.Tool()):
 		# load emme network
 		network = eb.scenario(100).get_network()
 
-		# create on-the-fly attributes (temporary)
+		# create extra network attributes (maybe temporary)
 
-		# link modes as string
+		# link modes_str attribute
 		network.create_attribute("LINK", "mode_str")
 		for link in network.links():
 			link.mode_str = "".join([m.id for m in link.modes])
 
-		# link isTransit flag
+		# link isTransit flag attribute
 		network.create_attribute("LINK", "isTransit")
 		transit_modes = set([m for m in network.modes() if m.type == "TRANSIT"])
 		for link in network.links():
 			link.isTransit = bool(link.modes.intersection(transit_modes))
 
-		# isFirst and isLast flags
+		# transit segment isFirst and isLast flags attributes
 		network.create_attribute("TRANSIT_SEGMENT", "isFirst", False)
 		network.create_attribute("TRANSIT_SEGMENT", "isLast", False)
 		for line in network.transit_lines():
@@ -187,18 +192,53 @@ class input_checker(_m.Tool()):
 			first_seg.isFirst = True
 			last_seg.isLast = True
 
-		# node isCentroid flag
+		# node isCentroid flag attribute
 		network.create_attribute("NODE", "isCentroid", False)
 		centroids = [c for c in network.nodes() if c.is_centroid]
 		for node in network.nodes():
 			node.isCentroid = bool(node in centroids)
 
-		# node inLink and outLink attributes
+		# node numInLinks and numOutLinks attributes
 		network.create_attribute("NODE", "numInLinks")
 		network.create_attribute("NODE", "numOutLinks")
 		for node in network.nodes():
 			node.numInLinks = len(list(node.incoming_links()))
 			node.numOutLinks = len(list(node.outgoing_links()))
+
+		# node hasLocalConnection flag attribute
+		class BreakLoop (Exception):
+			pass
+
+		network.create_attribute("NODE", "hasLocalConnection", False)
+		for node in network.centroids():
+			try:
+				for zone_connector in node.incoming_links():
+					for local_link in zone_connector.i_node.incoming_links():
+						if local_link["@lane_restriction"] == 1.0:
+							node.hasLocalConnection = True
+							raise BreakLoop("")
+			except:
+				pass
+
+		# transit line hasTAP flag attribute
+		network.create_attribute("TRANSIT_LINE", "hasTAP", False)
+		for line in network.transit_lines():
+			has_first_tap = False
+			has_last_tap = False
+			for link in line.segment(0).i_node.outgoing_links():
+				if link.j_node["@tap_id"] > 0:
+					has_first_tap = True
+					break
+			for link in line.segment(-2).j_node.outgoing_links():
+				if link.j_node["@tap_id"] > 0:
+					has_last_tap = True
+					break
+			line.hasTAP = has_first_tap and has_last_tap
+
+		# link names attribute
+		network.create_attribute("LINK", "linkNames")
+		for link in network.links():
+			link.linkNames = str(link['#name'] + "," + link['#name_from'] + "," + link['#name_to'])
 
 		def get_emme_object(emme_network, emme_network_object, fields_to_export):
 			# Emme network attribute and object names
@@ -206,7 +246,8 @@ class input_checker(_m.Tool()):
 			'NODE':'nodes', 
 			'LINK':'links', 
 			'TRANSIT_SEGMENT':'transit_segments', 
-			'TRANSIT_LINE':'transit_lines'
+			'TRANSIT_LINE':'transit_lines',
+			'CENTROID':'centroids'
 			}
 
 			# read-in entire emme network object as a list
@@ -216,7 +257,10 @@ class input_checker(_m.Tool()):
 			# get list of network object attributes
 			obj_attr = []
 			if fields_to_export[0] in ['all','All','ALL']:
-				obj_attr = emme_network.attributes(emme_network_object)
+				if emme_network_object == 'CENTROID':
+					obj_attr = emme_network.attributes('NODE')
+				else:
+					obj_attr = emme_network.attributes(emme_network_object)
 			else:
 				obj_attr = fields_to_export
 				
@@ -259,7 +303,7 @@ class input_checker(_m.Tool()):
 					df = dbf.to_dataframe()
 					self.inputs[table_name] = df
 
-		# add scenario year as variable input
+		# add scenario year
 		self.inputs['scenario'] = self.scenario_df
 
 	def checks(self):
@@ -276,6 +320,40 @@ class input_checker(_m.Tool()):
 
 		# remove all commented checks from the checks list
 		self.inputs_checks = self.inputs_checks.loc[[not i for i in (self.inputs_checks['Test'].str.startswith('#'))]]
+
+		# perform calculations and add user-defined data frame subsets
+		for item, row in self.inputs_checks.iterrows():
+
+			test = row['Test']
+			table = row['Input_Table']
+			id_col = row['Input_ID_Column']
+			expr = row['Expression']
+			test_vals = row['Test_Vals']
+			if not (pd.isnull(row['Test_Vals'])):
+				test_vals = test_vals.split(',')
+				test_vals = [txt.strip() for txt in test_vals]
+			test_type = row['Type']
+			Severity = row['Severity']
+			stat_expr = row['Report_Statistic']
+
+			if test_type == 'Calculation':
+
+				try: 					
+					calc_expr = test + ' = ' + expr
+					exec(calc_expr, {}, calc_dict)
+					calc_out = eval(expr, calc_dict)
+				except Exception as error:
+					print('An error occurred with the calculation: {}'.format(test))
+					raise
+
+				if str(type(calc_out)) == "<class 'pandas.core.frame.DataFrame'>":
+					print('added '+ row['Test'] + ' as new DataFrame input')
+					self.inputs[row['Test']] = calc_out
+					self.inputs_list = self.inputs_list.append({'Input_Table': row['Test'],'Property_Token':'NA','Emme_Object':'NA', \
+						'Fields':'NA','Column_Map':'NA','Input_Description':'NA'}, ignore_index = True)
+					self.inputs_checks = self.inputs_checks.append({'Test':test, 'Input_Table': table, 'Input_ID_Column':id_col, 'Severity':Severity, \
+						'Type':test_type, 'Expression': expr, 'Test_Vals':test_vals, 'Report_Statistic':stat_expr, 'Test_Description': row['Test_Description']}, \
+						ignore_index = True)
 
 		# loop through list of checks and conduct all checks
 		# checks must evaluate to True if inputs are correct
@@ -300,8 +378,9 @@ class input_checker(_m.Tool()):
 					# perform test
 					try:
 						out = eval(expr, calc_dict)
-					except:
+					except Exception as error:
 						print('An error occurred with the check: {}'.format(test))
+						raise
 
 					# check if test result is a series
 					if str(type(out)) == "<class 'pandas.core.series.Series'>":
@@ -315,10 +394,7 @@ class input_checker(_m.Tool()):
 						error_id_list = eval(error_expr)
 
 						# report first 25 problem IDs in the log
-						if error_id_list.size > 25:
-							self.problem_ids[test] = error_id_list.iloc[range(25)]
-						else:
-							self.problem_ids[test] = error_id_list if error_id_list.size > 0 else []
+						self.problem_ids[test] = error_id_list if error_id_list.size > 0 else []
 
 						# compute report statistics
 						if (pd.isnull(stat_expr)):
@@ -340,8 +416,9 @@ class input_checker(_m.Tool()):
 						# perform test (test result must not be of type Series)
 						try:
 							out = eval(expr)
-						except:
+						except Exception as error:
 							print('An error occurred with the check: {}'.format(test))
+							raise 
 
 						# compute report statistic
 						if (pd.isnull(stat_expr)):
@@ -358,8 +435,9 @@ class input_checker(_m.Tool()):
 				try: 
 					calc_expr = test + ' = ' + expr
 					exec(calc_expr, {}, calc_dict)
-				except:
+				except Exception as error:
 					print('An error occurred with the calculation: {}'.format(test))
+					raise
 
 	def prop_file_paths(self):
 		prop_files = self.inputs_list[['Input_Table','Property_Token']].dropna()
@@ -377,7 +455,7 @@ class input_checker(_m.Tool()):
 
 	def write_log(self):
 		# function to write out the input checker log file
-		# there are four blocks:
+		# there are four blocks
 		#   - Introduction
 		#	- Summary of checks
 		#   - Action Required: FATAL, LOGICAL, WARNINGS
@@ -438,7 +516,7 @@ class input_checker(_m.Tool()):
 		# get count of all FATAL failures
 		self.num_fatal = checks_df.result[(checks_df.Severity=='Fatal') & (checks_df.reverse_result)].count()
 		
-		# get all LOGICAL failures
+		# get count of all LOGICAL failures
 		self.num_logical = checks_df.result[(checks_df.Severity=='Logical') & (checks_df.reverse_result)].count()
 		self.logical_fails = checks_df[(checks_df.Severity=='Logical') & (checks_df.reverse_result)]
 		
@@ -478,13 +556,16 @@ class input_checker(_m.Tool()):
 			fh.write("\r\n\r\n\t TEST RESULT: " + ('PASSED' if row['result'] else 'FAILED'))
 
 			# display problem IDs for failed column checks
+			wrapper = textwrap.TextWrapper(width = 70)
 			if (not row['result']) & (len(problem_ids)>0) :
-				fh.write("\r\n\t TEST failed for following values of ID Column: " + row['Input_ID_Column'] + " (only upto 25 IDs displayed)")
-				fh.write("\r\n\t " + row['Input_ID_Column'] + ": " + ','.join(map(str, problem_ids[0:25])))
+				fh.write("\r\n\t TEST failed for following values of ID Column: " + row['Input_ID_Column'] + " (only up to 25 IDs displayed)")
+				fh.write("\r\n\t " + row['Input_ID_Column'] + ": " + "\r\n\t " + "\r\n\t ".join(wrapper.wrap(text = ", ".join(map(str, problem_ids[0:25])))))
 				if not (pd.isnull(row['Report_Statistic'])):
 					this_report_stat = self.report_stat[row['Test']]
-					fh.write("\r\n\t Test Statistics: " + ','.join(map(str, this_report_stat[0:25])))
-				fh.write("\r\n\t Total number of failures: " + str(len(problem_ids)))
+					fh.write("\r\n\t Test Statistics: " + "\r\n\t " + "\r\n\t ".join(wrapper.wrap(text = ", ".join(map(str, this_report_stat[0:25])))))
+				fh.write("\r\n\t Total number of failures: " + str(len(self.problem_ids[row['Test']])))
+				if ((len(self.problem_ids[row['Test']])) > 25) and (row['Severity'] == 'Logical'):
+					fh.write("\r\n\t Open {} for complete list of failed Logical failures.".format(self.logical_log_path))
 			else:
 				if not (pd.isnull(row['Report_Statistic'])):
 					fh.write("\r\n\t Test Statistic: " + str(self.report_stat[row['Test']]))
@@ -557,13 +638,128 @@ class input_checker(_m.Tool()):
 			
 		f.close()
 
+	def write_logical_log(self):
+		# function to write out the complete list of Logical failures
+
+		# combine results, inputs_checks and inputs_list
+		self.inputs_checks['result'] = self.inputs_checks['Test'].map(self.results)
+		checks_df = pd.merge(self.inputs_checks, self.inputs_list, on='Input_Table')
+		checks_df = checks_df[checks_df.Type=='Test']
+		checks_df['reverse_result'] = [not i for i in checks_df.result]
+		
+		# get count of all LOGICAL failures
+		self.num_logical = checks_df.result[(checks_df.Severity=='Logical') & (checks_df.reverse_result)].count()
+		self.logical_fails = checks_df[(checks_df.Severity=='Logical') & (checks_df.reverse_result)]
+
+		if self.num_logical > 0:
+			log_fail_id_tally = 0
+			for item, row in self.logical_fails.iterrows():
+				problem_ids = self.problem_ids[row['Test']]
+				if len(problem_ids) > 0:
+					log_fail_id_tally += 1
+		
+		if log_fail_id_tally > 0:
+
+			# create log file
+			now = datetime.datetime.now()
+
+			self.logical_log_path = _join(self.input_checker_path, ('completeLogicalFails_' + now.strftime("[%Y-%m-%d]") + '.txt'))
+			f = open(self.logical_log_path, 'wb')
+			
+			# define re-usable elements
+			seperator1 = '###########################################################'
+			seperator2 = '***********************************************************'
+			
+			# write out Header
+			f.write(seperator1 + seperator1 + "\r\n")
+			f.write(seperator1 + seperator1 + "\r\n\r\n")
+			f.write("\t SANDAG ABM Input Checker Logical Failures Complete List \r\n")
+			f.write("\t _______________________________________________________ \r\n\r\n\r\n")
+			f.write("\t Created on: " + now.strftime("%Y-%m-%d %H:%M") + "\r\n\r\n")
+			f.write("\t Notes:-\r\n")		
+			f.write("\t The SANDAG ABM Input Checker performs various QA/QC checks on SANDAG ABM inputs as specified by the user.\r\n")
+			f.write("\t The Input Checker allows the user to specify three severity levels for each QA/QC check:\r\n\r\n")
+			f.write("\t 1) FATAL  2) LOGICAL  3) WARNING\r\n\r\n")
+			f.write("\t This file provides the complete list of failed checks for checks of severity type Logical. \r\n")
+			f.write(seperator1 + seperator1 + "\r\n")
+			f.write(seperator1 + seperator1 + "\r\n\r\n\r\n\r\n")
+
+			# write total number of failed logical checks
+			f.write('\r\n\r\n' + seperator2 + seperator2 + "\r\n")
+			f.write(seperator2 + seperator2 + "\r\n\r\n")
+			f.write('\t' + "TALLY OF FAILED CHECKS \r\n")
+			f.write('\t' + "---------------------- \r\n\r\n")
+			f.write(seperator2 + seperator2 + "\r\n")
+			f.write(seperator2 + seperator2 + "\r\n\r\n\t")
+			f.write('\r\n\t Number of Logical Errors: ' + str(self.num_logical))
+
+			def write_logical_check_log(self, fh, row):
+				# define constants
+				seperator2 = '-----------------------------------------------------------'
+
+				# integerize problem ID list
+				problem_ids = self.problem_ids[row['Test']]
+				#problem_ids = [int(x) for x in problem_ids]
+
+				# write check summary
+				fh.write('\r\n\r\n' + seperator2 + seperator2)
+				fh.write("\r\n\t Input File Name: " + ('NA' if not pd.isnull(row['Emme_Object']) else 
+					(self.prop_input_paths[row['Input_Table']].rsplit('/', 1)[-1])))
+				fh.write("\r\n\t Input File Location: " + ('NA' if not pd.isnull(row['Emme_Object']) else 
+					(_join(self.input_checker_path, self.prop_input_paths[row['Input_Table']].replace('/','\\')))))
+				fh.write("\r\n\t Emme Object: " + (row['Emme_Object'] if not pd.isnull(row['Emme_Object']) else 'NA'))
+				fh.write("\r\n\t Input Description: " + (row['Input_Description'] if not pd.isnull(row['Input_Description']) else ""))
+				fh.write("\r\n\t Test Name: " + row['Test'])
+				fh.write("\r\n\t Test_Description: " + (row['Test_Description'] if not pd.isnull(row['Test_Description']) else ""))
+				fh.write("\r\n\t Test Severity: " + row['Severity'])
+				fh.write("\r\n\r\n\t TEST RESULT: " + ('PASSED' if row['result'] else 'FAILED'))
+
+				# display problem IDs for failed column checks
+				wrapper = textwrap.TextWrapper(width = 70)
+				if (not row['result']) & (len(problem_ids)>0) :
+					fh.write("\r\n\t TEST failed for following values of ID Column: " + row['Input_ID_Column'])
+					fh.write("\r\n\t " + row['Input_ID_Column'] + ": " + "\r\n\t " + "\r\n\t ".join(wrapper.wrap(text = ", ".join(map(str, problem_ids)))))
+					if not (pd.isnull(row['Report_Statistic'])):
+						this_report_stat = self.report_stat[row['Test']]
+						fh.write("\r\n\t Test Statistics: " + "\r\n\t " + "\r\n\t ".join(wrapper.wrap(text = ", ".join(map(str, this_report_stat)))))
+					fh.write("\r\n\t Total number of failures: " + str(len(self.problem_ids[row['Test']])))
+				else:
+					if not (pd.isnull(row['Report_Statistic'])):
+						fh.write("\r\n\t Test Statistic: " + str(self.report_stat[row['Test']]))
+
+				# display result for each test val if it was specified
+				if not (pd.isnull(row['Test_Vals'])):
+					fh.write("\r\n\t TEST results for each test val")
+					result_tuples = zip(row['Test_Vals'].split(","), self.result_list[row['Test']])
+					fh.write("\r\n\t ")
+					fh.write(','.join('[{} - {}]'.format(x[0],x[1]) for x in result_tuples))
+					
+				fh.write("\r\n" + seperator2 + seperator2 + "\r\n\r\n")
+			
+			# write out ACTION REQUIRED section if needed
+			if self.num_logical > 0:
+				logical_checks = checks_df[(checks_df.Severity=='Logical') & (checks_df.reverse_result)]
+				f.write('\r\n\r\n' + seperator2 + seperator2 + "\r\n")
+				f.write(seperator2 + seperator2 + "\r\n\r\n")
+				f.write('\t' + "LOG OF ALL FAILED LOGICAL CHECKS \r\n")
+				f.write('\t' + "-------------------------------- \r\n\r\n")
+				f.write(seperator2 + seperator2 + "\r\n")
+				f.write(seperator2 + seperator2 + "\r\n")
+				
+				#write out log for each check
+				for item, row in logical_checks.iterrows():
+					if len(self.problem_ids[row['Test']]) > 25:
+						write_logical_check_log(self, f, row)
+
+			f.close()
+
 	def check_logical(self):
 		if self.num_logical > 0:
 			# raise exception for each logical check fail
 			for item, row in self.logical_fails.iterrows():
 				answer = dialog.alert_question(
 					message = "The following Logical check resulted in at least 1 error: {} \n Open {} for details. \
-					 \n\n Click OK to continue or Cancel to stop input checker.".format(row['Test'], self.log_path),
+					 \n\n Click OK to continue or Cancel to stop run.".format(row['Test'], self.log_path),
 					title = "Logical Check Error",
 					answers = [("OK", dialog.YES_ROLE), ("Cancel", dialog.REJECT_ROLE)]
 					)
