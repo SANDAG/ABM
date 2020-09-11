15edcdd4e28853a8f08e8b7dcd86571efdb8ab59
Cundo Arellano <cundo.a@gmail.com>
2020-09-11 14:33:07 -0700

    properties  - fixed indentation error under save method

--- src/main/emme/toolbox/utilities/properties.py
+++ src/main/emme/toolbox/utilities/properties.py
@@ -501,11 +501,11 @@ class Properties(object):
     def save(self, path=None):
         if not path:
             path = self._path
-        # check for possible interference if user edits the 
-        # properties files directly while it is already open in Modeller
-        timestamp = os.path.getmtime(path)
-        if timestamp != self._timestamp:
-            raise Exception("%s file conflict - edited externally after loading" % path)
+            # check for possible interference if user edits the 
+            # properties files directly while it is already open in Modeller
+            timestamp = os.path.getmtime(path)
+            if timestamp != self._timestamp:
+                raise Exception("%s file conflict - edited externally after loading" % path)
         self["SavedFrom"] = "Emme Modeller properties writer Process ID %s" % os.getpid()
         self["SavedLast"] = time.strftime("%b-%d-%Y %H:%M:%S")
         with open(path, 'w') as f:
