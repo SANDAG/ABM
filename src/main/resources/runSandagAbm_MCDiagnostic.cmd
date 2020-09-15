de57aefd9b1c267381e1fd2b57d231979fbce6bf
Cundo Arellano <cundo.a@gmail.com>
2020-09-15 09:38:38 -0700

    mode choice diagnostic:  - java processes close when finished

--- src/main/resources/runSandagAbm_MCDiagnostic.cmd
+++ src/main/resources/runSandagAbm_MCDiagnostic.cmd
@@ -50,3 +50,6 @@ rem ### restore saved environment variable values, and change back to original c
 set JAVA_PATH=%OLDJAVAPATH%
 set PATH=%OLDPATH%
 set CLASSPATH=%OLDCLASSPATH%
+
+rem kill java tasks
+taskkill /F /IM java.exe
