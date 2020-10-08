a61671fcefbffc9738243fb82dd9ee240273044c
Antoine Grapperon <antoine.grapperon@gmail.com>
2020-10-07 21:53:07 -0400

    Add makeLastHighwayAssignmentStochastic

--- src/main/emme/toolbox/utilities/properties.py
+++ src/main/emme/toolbox/utilities/properties.py
@@ -75,6 +75,7 @@ class PropertiesSetter(object):
     skipTripTableCreation_3 = _m.Attribute(bool)
 
     skipFinalHighwayAssignment = _m.Attribute(bool)
+    makeFinalHighwayAssignmentStochastic = _m.Attribute(bool)
     skipFinalTransitAssignment = _m.Attribute(bool)
     skipVisualizer = _m.Attribute(bool)
     skipDataExport = _m.Attribute(bool)
@@ -134,7 +135,7 @@ class PropertiesSetter(object):
             "skipHighwayAssignment", "skipTransitSkimming", "skipTransponderExport", "skipCoreABM", "skipOtherSimulateModel", "skipMAASModel","skipCTM", 
             "skipEI", "skipExternalExternal", "skipTruck", "skipTripTableCreation", "skipFinalHighwayAssignment", 
             "skipFinalTransitAssignment", "skipVisualizer", "skipDataExport", "skipDataLoadRequest", 
-            "skipDeleteIntermediateFiles")
+            "skipDeleteIntermediateFiles", 'makeFinalHighwayAssignmentStochastic')
         self._properties = None
 
     def add_properties_interface(self, pb, disclosure=False):
@@ -195,6 +196,7 @@ class PropertiesSetter(object):
         ]
         skip_final_items = [
             ("skipFinalHighwayAssignment",  "Skip final highway assignments"),
+            ("makeFinalHighwayAssignmentStochastic",  "Make final highway assignments stochastic"),
             ("skipFinalTransitAssignment",  "Skip final transit assignments"),
             ("skipVisualizer",              "Skip running visualizer"),
             ("skipDataExport",              "Skip data export"),
@@ -325,6 +327,7 @@ class PropertiesSetter(object):
         self.skipTripTableCreation = props.get("RunModel.skipTripTableCreation", [False, False, False])
 
         self.skipFinalHighwayAssignment = props.get("RunModel.skipFinalHighwayAssignment", False)
+        self.makeFinalHighwayAssignmentStochastic = props.get("RunModel.makeFinalHighwayAssignmentStochastic", False)
         self.skipFinalTransitAssignment = props.get("RunModel.skipFinalTransitAssignment", False) 
         self.skipVisualizer = props.get("RunModel.skipVisualizer", False)
         self.skipDataExport = props.get("RunModel.skipDataExport", False)
@@ -361,6 +364,7 @@ class PropertiesSetter(object):
         props["RunModel.skipTripTableCreation"] = self.skipTripTableCreation
 
         props["RunModel.skipFinalHighwayAssignment"] = self.skipFinalHighwayAssignment
+        props["RunModel.makeFinalHighwayAssignmentStochastic"] = self.makeFinalHighwayAssignmentStochastic
         props["RunModel.skipFinalTransitAssignment"] = self.skipFinalTransitAssignment
         props["RunModel.skipVisualizer"] = self.skipVisualizer
         props["RunModel.skipDataExport"] = self.skipDataExport
