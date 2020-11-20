e21ad36ef6598381d9b89e5f6ebb735be90c402d
wusun2 <Wu.Sun@sandag.org>
2020-11-20 11:33:39 -0800

    Update GUI to 14.2.2

--- src/main/python/pythonGUI/createStudyAndScenario.py
+++ src/main/python/pythonGUI/createStudyAndScenario.py
@@ -54,8 +54,8 @@ class CreateScenarioGUI(Tkinter.Frame):
 
             Tkinter.Label(body, text=u"Version", font=("Helvetica", 8, 'bold')).grid(row=8)
             var = StringVar(root)
-            self.version="version_14_2_1"
-            optionList=["version_14_2_1"]
+            self.version="version_14_2_2"
+            optionList=["version_14_2_2"]
             option=Tkinter.OptionMenu(body,var,*optionList,command=self.setversion)
             option.config(width=50)
             option.grid(row=8, column=1)
