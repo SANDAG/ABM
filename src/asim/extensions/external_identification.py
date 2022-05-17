ae526c28a04c2241ea4b240784a4b30be56cf8be
dhensle <hensle93@gmail.com>
2022-05-17 15:06:00 -0700

    external model code

--- src/asim/extensions/external_identification.py
+++ src/asim/extensions/external_identification.py
@@ -18,27 +18,23 @@ logger = logging.getLogger(__name__)
 
 def determine_closest_external_station(choosers, skim_dict, origin_col='home_zone_id'):
 
-    unique_home_zones = choosers[origin_col].unique()
+    unique_origin_zones = choosers[origin_col].unique()
     landuse = inject.get_table('land_use').to_frame()
-    print(landuse)
     ext_zones = landuse[landuse.external_MAZ > 0].index.to_numpy()
-    print(ext_zones)
 
     choosers['closest_external_zone'] = -1
-    choosers['dist_to_external_zone'] = 0
+    choosers['dist_to_external_zone'] = 0.0
 
     for origin_zone in unique_origin_zones:
-        # for ext_zone in ext_zones:
-        #     print('home: ', origin_zone, '\t external: ', ext_zone)
-        #     print('home type: ', type(origin_zone), '\t external type: ', type(ext_zone))
-        #     print(skim_dict.lookup([origin_zone], [ext_zone], 'DIST'))
-        print(unique_origin_zones)
-        zone_distances = skim_dict.lookup([origin_zone], ext_zones, 'DIST')
-        # zone_distances = [skim_dict.lookup([origin_zone], [ext_zone], 'DIST') for ext_zone in ext_zones]
-        # closest_zone_idx = np.argmin(skim_dict.lookup([origin_zone], ext_zones, 'DIST'))
-        closest_zone = ext_zones[closest_zone_idx]
-        dist_to_closest_zone = zone_distances[closest_zone_idx]
-        print("closest_zone: ", closest_zone, '\t Distance:', dist_to_closest_zone)
+        # FIXME not sure why I get an unhashable type: numpy.ndarray when trying to do them all at once...
+        # zone_distances = skim_dict.lookup(origin_zone, ext_zones, 'DIST')
+        # doing it one at a time instead
+        zone_distances = [skim_dict.lookup([origin_zone], [ext_zone], 'DIST') for ext_zone in ext_zones]
+
+        closest_zone_idx = np.argmin(zone_distances)
+        closest_zone = int(ext_zones[closest_zone_idx])
+        dist_to_closest_zone = zone_distances[closest_zone_idx][0]
+
         choosers.loc[choosers[origin_col] == origin_zone,
             'closest_external_zone'] = closest_zone
         choosers.loc[choosers[origin_col] == origin_zone,
@@ -55,10 +51,11 @@ def external_identification(model_settings, estimator, choosers, network_los, ch
     locals_d = {}
     if constants is not None:
         locals_d.update(constants)
+    locals_d.update({'land_use': inject.get_table('land_use').to_frame()})
+
     skim_dict = network_los.get_default_skim_dict()
     # print('skim_dict', skim_dict)
-    # locals_d.update
-    # choosers = determine_closest_external_station(choosers, skim_dict)
+    choosers = determine_closest_external_station(choosers, skim_dict)
 
     # - preprocessor
     preprocessor_settings = model_settings.get('preprocessor', None)
@@ -123,7 +120,7 @@ def external_worker_identification(
     internal_col_name = model_settings['INTERNAL_COL_NAME']
 
     persons = persons.to_frame()
-    persons[external_col_name] = choices.reindex(persons.index).fillna(0).astype(bool)
+    persons[external_col_name] = (choices == 0).reindex(persons.index).fillna(False).astype(bool)
     persons[internal_col_name] = (persons[filter_col] & ~persons[external_col_name])
 
     if estimator:
@@ -170,7 +167,7 @@ def external_student_identification(
     internal_col_name = model_settings['INTERNAL_COL_NAME']
 
     persons = persons.to_frame()
-    persons[external_col_name] = choices.reindex(persons.index).fillna(0).astype(bool)
+    persons[external_col_name] = (choices == 0).reindex(persons.index).fillna(False).astype(bool)
     persons[internal_col_name] = (persons[filter_col] & ~persons[external_col_name])
 
     if estimator:
@@ -188,9 +185,35 @@ def external_student_identification(
                          label=trace_label,
                          warn_if_empty=True)
 
+
+def set_external_tour_variables(tours, choices, model_settings, trace_label):
+    """
+    Set the internal and external tour indicator columns in the tours file
+    """
+    external_col_name = model_settings['EXTERNAL_COL_NAME']
+    internal_col_name = model_settings['INTERNAL_COL_NAME']
+
+    tours = tours.to_frame()
+
+    tours.loc[choices.index, external_col_name] = (choices == 0).reindex(
+        tours.index).fillna(False).astype(bool)
+    tours.loc[choices.index, internal_col_name] = np.where(
+        tours.loc[choices.index, external_col_name], False, True)
+
+    # - annotate tours table
+    if 'annotate_tours' in model_settings:
+        expressions.assign_columns(
+            df=tours,
+            model_settings=model_settings.get('annotate_tours'),
+            trace_label=tracing.extend_trace_label(trace_label, 'annotate_tours'))
+
+    return tours
+
+
+
 @inject.step()
-def external_nonmandatory_identification(
-        persons_merged, persons, network_los,
+def external_non_mandatory_identification(
+        tours_merged, tours, network_los,
         chunk_size, trace_hh_id):
     """
     This model predicts the whether a non-mandatory tour is external.
@@ -207,17 +230,12 @@ def external_nonmandatory_identification(
     estimator = estimation.manager.begin_estimation(trace_label)
 
     choosers = tours_merged.to_frame()
-    filter_col = model_settings.get('CHOOSER_FILTER_COLUMN_NAME')
-    choosers = choosers[choosers[filter_col]]
+    choosers = choosers[choosers['tour_category'] == 'non_mandatory']
 
-    choices = external_identification(model_settings, estimator, choosers, network_los, chunk_size, trace_label)
+    choices = external_identification(
+        model_settings, estimator, choosers, network_los, chunk_size, trace_label)
 
-    external_col_name = model_settings['EXTERNAL_COL_NAME']
-    internal_col_name = model_settings['INTERNAL_COL_NAME']
-
-    tours = tours.to_frame()
-    tours[external_col_name] = choices.reindex(tours.index).fillna(0).astype(bool)
-    tours[internal_col_name] = (tours[filter_col] & ~tours[external_col_name])
+    tours = set_external_tour_variables(tours, choices, model_settings, trace_label)
 
     if estimator:
         estimator.write_choices(choices)
@@ -227,9 +245,60 @@ def external_nonmandatory_identification(
 
     pipeline.replace_table("tours", tours)
 
+    external_col_name = model_settings['EXTERNAL_COL_NAME']
     tracing.print_summary(external_col_name, tours[external_col_name], value_counts=True)
 
     if trace_hh_id:
-        tracing.trace_df(persons,
+        tracing.trace_df(tours,
+                         label=trace_label,
+                         warn_if_empty=True)
+
+
+
+@inject.step()
+def external_joint_tour_identification(
+        tours_merged, tours, network_los,
+        chunk_size, trace_hh_id):
+    """
+    This model predicts the whether a joint tour is external.
+    The output from this model is TRUE (if external) or FALSE (if internal).
+
+    The main interface to the external student model is the external_nonmandatory_identification() function.
+    This function is registered as an orca step in the example Pipeline.
+    """
+
+    trace_label = 'external_joint_tour_identification'
+    model_settings_file_name = 'external_joint_tour_identification.yaml'
+    model_settings = config.read_model_settings(model_settings_file_name)
+
+    estimator = estimation.manager.begin_estimation(trace_label)
+
+    choosers = tours_merged.to_frame()
+    choosers = choosers[choosers['tour_category'] == 'joint']
+
+    # - if no choosers
+    if choosers.shape[0] > 0:
+        choices = external_identification(
+            model_settings, estimator, choosers, network_los, chunk_size, trace_label)
+    else:
+        # everything is internal, still want to set internal or external columns in df
+        choices = pd.Series(1, index=choosers.index)
+        tracing.no_results(trace_label)
+
+    tours = set_external_tour_variables(tours, choices, model_settings, trace_label)
+
+    if estimator:
+        estimator.write_choices(choices)
+        choices = estimator.get_survey_values(choices, 'tours', trace_label)
+        estimator.write_override_choices(choices)
+        estimator.end_estimation()
+
+    pipeline.replace_table("tours", tours)
+
+    external_col_name = model_settings['EXTERNAL_COL_NAME']
+    tracing.print_summary(external_col_name, tours[external_col_name], value_counts=True)
+
+    if trace_hh_id:
+        tracing.trace_df(tours,
                          label=trace_label,
                          warn_if_empty=True)
--- src/asim/extensions/external_location_choice.py
+++ src/asim/extensions/external_location_choice.py
@@ -12,7 +12,12 @@ from activitysim.core import inject
 from activitysim.core import expressions
 
 from activitysim.abm.models.util import estimation
+from activitysim.abm.models.util import tour_destination
 from activitysim.abm.models.location_choice import iterate_location_choice
+
+from activitysim.core.util import assign_in_place
+
+
 
 logger = logging.getLogger(__name__)
 
@@ -81,3 +86,178 @@ def external_workplace_location(
 
     if estimator:
         estimator.end_estimation()
+
+
+@inject.step()
+def external_non_mandatory_destination(
+        tours,
+        persons_merged,
+        network_los,
+        chunk_size,
+        trace_hh_id):
+
+    """
+    Given the tour generation from the above, each tour needs to have a
+    destination, so in this case tours are the choosers (with the associated
+    person that's making the tour)
+    """
+
+    trace_label = 'external_non_mandatory_destination'
+    model_settings_file_name = 'external_non_mandatory_destination.yaml'
+    model_settings = config.read_model_settings(model_settings_file_name)
+
+    logsum_column_name = model_settings.get('DEST_CHOICE_LOGSUM_COLUMN_NAME')
+    want_logsums = logsum_column_name is not None
+
+    sample_table_name = model_settings.get('DEST_CHOICE_SAMPLE_TABLE_NAME')
+    want_sample_table = config.setting('want_dest_choice_sample_tables') and sample_table_name is not None
+
+    tours = tours.to_frame()
+
+    persons_merged = persons_merged.to_frame()
+
+    # choosers are tours - in a sense tours are choosing their destination
+    non_mandatory_ext_tours = tours[
+        (tours.tour_category == 'non_mandatory')
+        & (tours.is_external_tour)]
+
+    # if no external non-mandatory tours
+    if non_mandatory_ext_tours.shape[0] == 0:
+        tracing.no_results(trace_label)
+        return
+
+    estimator = estimation.manager.begin_estimation('external_non_mandatory_destination')
+    if estimator:
+        estimator.write_coefficients(model_settings=model_settings)
+        # estimator.write_spec(model_settings, tag='SAMPLE_SPEC')
+        estimator.write_spec(model_settings, tag='SPEC')
+        estimator.set_alt_id(model_settings["ALT_DEST_COL_NAME"])
+        estimator.write_table(inject.get_injectable('size_terms'), 'size_terms', append=False)
+        estimator.write_table(inject.get_table('land_use').to_frame(), 'landuse', append=False)
+        estimator.write_model_settings(model_settings, model_settings_file_name)
+
+    choices_df, save_sample_df = tour_destination.run_tour_destination(
+        non_mandatory_ext_tours,
+        persons_merged,
+        want_logsums,
+        want_sample_table,
+        model_settings,
+        network_los,
+        estimator,
+        chunk_size, trace_hh_id, trace_label)
+
+    if estimator:
+        estimator.write_choices(choices_df.choice)
+        choices_df.choice = estimator.get_survey_values(choices_df.choice, 'tours', 'destination')
+        estimator.write_override_choices(choices_df.choice)
+        estimator.end_estimation()
+
+    non_mandatory_ext_tours['destination'] = choices_df.choice
+
+    assign_in_place(tours, non_mandatory_ext_tours[['destination']])
+
+    if want_logsums:
+        non_mandatory_ext_tours[logsum_column_name] = choices_df['logsum']
+        assign_in_place(tours, non_mandatory_ext_tours[[logsum_column_name]])
+
+    pipeline.replace_table("tours", tours)
+
+    if want_sample_table:
+        assert len(save_sample_df.index.get_level_values(0).unique()) == len(choices_df)
+        # save_sample_df.set_index(model_settings['ALT_DEST_COL_NAME'], append=True, inplace=True)
+        pipeline.extend_table(sample_table_name, save_sample_df)
+
+    if trace_hh_id:
+        tracing.trace_df(tours[tours.tour_category == 'non_mandatory'],
+                         label="external_non_mandatory_destination",
+                         slicer='person_id',
+                         index_label='tour',
+                         columns=None,
+                         warn_if_empty=True)
+
+
+@inject.step()
+def external_joint_tour_destination(
+        tours,
+        persons_merged,
+        network_los,
+        chunk_size,
+        trace_hh_id):
+
+    """
+    Given the tour generation from the above, each tour needs to have a
+    destination, so in this case tours are the choosers (with the associated
+    person that's making the tour)
+    """
+
+    trace_label = 'external_joint_tour_destination'
+    model_settings_file_name = 'external_joint_tour_destination.yaml'
+    model_settings = config.read_model_settings(model_settings_file_name)
+
+    logsum_column_name = model_settings.get('DEST_CHOICE_LOGSUM_COLUMN_NAME')
+    want_logsums = logsum_column_name is not None
+
+    sample_table_name = model_settings.get('DEST_CHOICE_SAMPLE_TABLE_NAME')
+    want_sample_table = config.setting('want_dest_choice_sample_tables') and sample_table_name is not None
+
+    tours = tours.to_frame()
+
+    persons_merged = persons_merged.to_frame()
+
+    joint_ext_tours = tours[
+        (tours.tour_category == 'joint')
+        & (tours.get('is_external_tour', False) == True)]  # get needed incase no joint tours
+
+    # if no external joint tours
+    if joint_ext_tours.shape[0] == 0:
+        tracing.no_results(trace_label)
+        return
+
+    estimator = estimation.manager.begin_estimation('external_joint_tour_destination')
+    if estimator:
+        estimator.write_coefficients(model_settings=model_settings)
+        # estimator.write_spec(model_settings, tag='SAMPLE_SPEC')
+        estimator.write_spec(model_settings, tag='SPEC')
+        estimator.set_alt_id(model_settings["ALT_DEST_COL_NAME"])
+        estimator.write_table(inject.get_injectable('size_terms'), 'size_terms', append=False)
+        estimator.write_table(inject.get_table('land_use').to_frame(), 'landuse', append=False)
+        estimator.write_model_settings(model_settings, model_settings_file_name)
+
+    choices_df, save_sample_df = tour_destination.run_tour_destination(
+        joint_ext_tours,
+        persons_merged,
+        want_logsums,
+        want_sample_table,
+        model_settings,
+        network_los,
+        estimator,
+        chunk_size, trace_hh_id, trace_label)
+
+    if estimator:
+        estimator.write_choices(choices_df.choice)
+        choices_df.choice = estimator.get_survey_values(choices_df.choice, 'tours', 'destination')
+        estimator.write_override_choices(choices_df.choice)
+        estimator.end_estimation()
+
+    joint_ext_tours['destination'] = choices_df.choice
+
+    assign_in_place(tours, joint_ext_tours[['destination']])
+
+    if want_logsums:
+        joint_ext_tours[logsum_column_name] = choices_df['logsum']
+        assign_in_place(tours, joint_ext_tours[[logsum_column_name]])
+
+    pipeline.replace_table("tours", tours)
+
+    if want_sample_table:
+        assert len(save_sample_df.index.get_level_values(0).unique()) == len(choices_df)
+        # save_sample_df.set_index(model_settings['ALT_DEST_COL_NAME'], append=True, inplace=True)
+        pipeline.extend_table(sample_table_name, save_sample_df)
+
+    if trace_hh_id:
+        tracing.trace_df(tours[tours.tour_category == 'non_mandatory'],
+                         label="external_joint_tour_destination",
+                         slicer='person_id',
+                         index_label='tour',
+                         columns=None,
+                         warn_if_empty=True)
