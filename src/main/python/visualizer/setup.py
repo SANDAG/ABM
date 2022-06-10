56eef053b98beef70047f1224a23f56e6e98d65b
SANDAGNET\jflo <jflo@sandag.org>
2022-06-10 15:29:02 -0700

    Added PowerBI Visualizer

---
+++ src/main/python/visualizer/setup.py
@@ -0,0 +1,53 @@
+import os
+from shutil import copy
+
+#run = r'T:/ABM/ABM_FY19/model_runs/ABM2Plus/v1221/2016_1422new_sxu'
+wd = os.path.dirname(os.path.dirname(os.path.realpath(__file__))).replace('\\', '/')
+run = os.path.dirname(wd)
+
+#import pdb
+#pdb.set_trace()
+
+#Set up data pipeline settings
+settings_file = os.path.join(wd, 'data_pipeliner', 'config', 'settings.yaml')
+processor_file = os.path.join(wd, 'data_pipeliner', 'config', 'processor.csv')
+config_path = os.path.dirname(settings_file)
+copy(os.path.join(wd, 'config', 'settings.yaml'), config_path)
+copy(os.path.join(wd, 'config', 'processor.csv'), config_path)
+copy(os.path.join(wd, 'config', 'expressions.csv'), config_path)
+
+#Settings
+f = open(settings_file, 'r')
+data = f.read()
+f.close()
+
+data = data.replace('[MODEL_RUN]', run)
+
+f = open(settings_file, 'w')
+f.write(data)
+f.close()
+
+#Processor
+f = open(processor_file, 'r')
+data = f.read()
+f.close()
+
+data = data.replace('[MODEL_RUN]', run)
+
+f = open(processor_file, 'w')
+f.write(data)
+f.close()
+
+#Set up combine settings
+combine_file = os.path.join(wd, 'combine.yaml')
+copy(os.path.join(wd, 'config', 'combine.yaml'), os.path.split(combine_file)[0])
+
+f = open(combine_file, 'r')
+data = f.read()
+f.close()
+
+data = data.replace('VISUALIZER_PATH', wd)
+
+f = open(combine_file, 'w')
+f.write(data)
+f.close()
