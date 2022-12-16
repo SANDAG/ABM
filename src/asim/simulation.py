69429cfccdf3f9589f6c53af69764c0b455448e1
David Hensle <davidh@sandag.org>
2022-12-16 10:36:23 -0800

    blacken

--- src/asim/simulation.py
+++ src/asim/simulation.py
@@ -11,14 +11,14 @@ from activitysim.cli.run import add_run_args, run
 
 import extensions
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+    warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
     parser = argparse.ArgumentParser()
     add_run_args(parser)
     args = parser.parse_args()
 
-    os.environ['MKL_NUM_THREADS'] = '1'
+    os.environ["MKL_NUM_THREADS"] = "1"
 
     sys.exit(run(args))
