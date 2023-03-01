5f61ce24919bc1860e067999e76d47ce0f6c581b
David Hensle <davidh@sandag.org>
2023-03-01 09:21:17 -0800

    run settings & documentation

--- src/asim/visualizer/visualizer/RunViz.bat
+++ src/asim/visualizer/visualizer/RunViz.bat
@@ -1,17 +1,20 @@
 :: Creating virtual Python environment
-call conda env create --file environment.yml
+:: if you need to create an environment, uncomment below conda calls:
+:: call conda env create --file environment.yml
 
-call conda activate abm3_viz
+:: call conda activate abm3_viz
 
 :: Run the data pipeline tool to summarize model results
-call python T:\data\tools\Data-Pipeline-Tool\run.py
+:: If path not found, try un-commenting below
+:: call python T:\data\tools\Data-Pipeline-Tool\run.py
+call python \\sandag.org\transdata\data\tools\Data-Pipeline-Tool\run.py
 if ERRORLEVEL 1 goto error
 
 :: Combine data pipeline outputs with survey results for comparison
 call python scripts\combine.py
 if ERRORLEVEL 1 goto error
 
-call conda deactivate
+:: call conda deactivate
 
 :success
 ECHO "Visualizer created successfully!"
