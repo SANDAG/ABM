e60691a483fd29804aa31b529dc7b96f312de36f
JoeJimFlood <joejimflood@gmail.com>
2023-09-05 12:18:17 -0700

    Changes to calibration visualizer, including bug fixes

--- src/asim/visualizer/visualizer/config/user_added_functions.py
+++ src/asim/visualizer/visualizer/config/user_added_functions.py
@@ -3,3 +3,17 @@ def get_escort_participant_list(escort_participants):
         return [int(participant) for participant in escort_participants.split('_')]
     else:
         return []
+
+def _get_vehicle_info(vehicle_attributes, index):
+    if type(vehicle_attributes) != str or vehicle_attributes == "non_hh_veh":
+        return vehicle_attributes
+    return vehicle_attributes.split("_")[index]
+
+def get_vehicle_body_type(vehicle_attributes):
+    return _get_vehicle_info(vehicle_attributes, 0)
+
+def get_vehicle_age(vehicle_attributes):
+    return _get_vehicle_info(vehicle_attributes, 1)
+
+def get_vehicle_fuel_type(vehicle_attributes):
+    return _get_vehicle_info(vehicle_attributes, 2)
