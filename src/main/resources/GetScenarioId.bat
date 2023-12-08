c99e71e6582dc2cb820a67eb226e0f9084be60b6
aber-sandag <aber@sandag.org>
2023-12-08 14:11:43 -0800

    update env variables

--- src/main/resources/GetScenarioId.bat
+++ src/main/resources/GetScenarioId.bat
@@ -3,4 +3,4 @@ ECHO OFF
 set SCENARIO_GUID=%1
 set SCENARIO_NAME=%2
 
-sqlcmd -d data-pipeline-engine -S sql-das-ads-dev-west.database.windows.net -U %db_pipeline_user% -P %db_pipeline_pw% -I -Q "EXEC [abm3].[GetScenarioId] '$(guid)', '$(name)'" -v guid=%SCENARIO_GUID% name=%SCENARIO_NAME%
+sqlcmd -d data-pipeline-engine -S sql-das-ads-dev-west.database.windows.net -U %ABM_METADATA_ID% -P %ABM_METADATA_SCENARIO% -I -Q "EXEC [abm3].[GetScenarioId] '$(guid)', '$(name)'" -v guid=%SCENARIO_GUID% name=%SCENARIO_NAME%
