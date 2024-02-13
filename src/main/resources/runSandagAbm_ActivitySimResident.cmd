7fbf89ead53788c82c29c54a2580d967010c4ea5
Michael Wehrmeyer <mwe@sandag.org>
2024-02-13 11:10:59 -0800

    resident batch file fails if res model fails
    [Cundo] So that the entire model crashes if the resident model crashes. What's been happening is that the resident model has been crashing but has continued to next steps and iterations

--- src/main/resources/runSandagAbm_ActivitySimResident.cmd
+++ src/main/resources/runSandagAbm_ActivitySimResident.cmd
@@ -77,6 +77,7 @@ CD ..
 :: Run simulation.py
 
 %PYTHON3% src/asim/simulation.py -s settings_mp.yaml -c src/asim/configs/resident -c src/asim/configs/common -d input -d output/skims -o output/resident
+if ERRORLEVEL 1 exit 2
 
 ::::::::::::::::::::::
 CD /d %ANACONDA2_DIR%\Scripts
