ab3c62ba732704c5055c852587f282d9cfbc64e1
Ali Etezady <58451076+aletzdy@users.noreply.github.com>
2024-02-22 13:53:18 -0800

    dynamicaly setting transit connector length in abm_prop

--- src/asim/scripts/scenarioManagement/scenManagement.py
+++ src/asim/scripts/scenarioManagement/scenManagement.py
@@ -1,43 +1,30 @@
 # %%
 import pandas as pd
-import ruamel.yaml as yamlru
-import sys
 import os
+import sys
+import utilities as util
 _join = os.path.join
 
 project_dir = sys.argv[1]
 scenYear = int(sys.argv[2])
 
 # %%
-def open_yaml(yaml_file):
-    with open(yaml_file, 'r') as stream:
-        contents = stream.read()
-        print(f"Contents of {yaml_file}: {contents}")
-        stream.seek(0)  # Reset the stream position to the start of the file
-        try:
-            return yamlru.load(stream, Loader=yamlru.RoundTripLoader)
-        except yamlru.YAMLError as exc:
-            print(exc)
-
-def write_yaml(yaml_file, yaml_dict):
-    with open(yaml_file, 'w') as outfile:
-        yamlru.dump(yaml_dict, outfile, Dumper=yamlru.RoundTripDumper)
-
-# %%
 configs_dir = _join(project_dir, r'src\asim\configs')
 paramByYear_dir = _join(project_dir, r'input\parametersByYears.csv')
+sandag_abm_prop_dir = _join(project_dir, r'conf\sandag_abm.properties')
+scripts_dir = _join(project_dir, r'src\asim\scripts')
 
 # %%
 paramByYear = pd.read_csv(paramByYear_dir)
 
 # %%
 #update airport parameters
-doc = open_yaml(_join(configs_dir, 'airport.CBX', 'preprocessing.yaml'))
+doc = util.open_yaml(_join(configs_dir, 'airport.CBX', 'preprocessing.yaml'))
 doc['tours']['num_enplanements'] = int(paramByYear.loc[paramByYear['year'] == scenYear, 'airport.CBX.enplanements'].values[0])
 doc['tours']['connecting'] = int(paramByYear.loc[paramByYear['year'] == scenYear, 'airport.CBX.connecting'].values[0])
 doc['tours']['airport_mgra'] = int(paramByYear.loc[paramByYear['year'] == scenYear, 'airport.CBX.airportMgra'].values[0])
-write_yaml(_join(configs_dir, 'airport.CBX', 'preprocessing.yaml'), doc)
-doc = open_yaml(_join(configs_dir, 'airport.CBX', 'trip_mode_choice.yaml'))
+util.write_yaml(_join(configs_dir, 'airport.CBX', 'preprocessing.yaml'), doc)
+doc = util.open_yaml(_join(configs_dir, 'airport.CBX', 'trip_mode_choice.yaml'))
 doc['CONSTANTS']['Taxi_baseFare'] = float(paramByYear.loc[paramByYear.year==scenYear, 'taxi.baseFare'].values[0])
 doc['CONSTANTS']['costPerMile'] = float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.fuel'].values[0]) + float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.maintenance'].values[0])
 doc['CONSTANTS']['Taxi_perMileFare'] = float(paramByYear.loc[paramByYear.year==scenYear, 'taxi.costPerMile'].values[0])
@@ -50,14 +37,14 @@ doc['CONSTANTS']['TNC_shared_baseFare'] = float(paramByYear.loc[paramByYear.year
 doc['CONSTANTS']['TNC_shared_costPerMile'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costPerMile'].values[0])
 doc['CONSTANTS']['TNC_shared_costPerMinute'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costPerMinute'].values[0])
 doc['CONSTANTS']['TNC_shared_costMinimum'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costMinimum'].values[0])
-write_yaml(_join(configs_dir, 'airport.CBX', 'trip_mode_choice.yaml'), doc)
+util.write_yaml(_join(configs_dir, 'airport.CBX', 'trip_mode_choice.yaml'), doc)
 
-doc = open_yaml(_join(configs_dir, 'airport.SAN', 'preprocessing.yaml'))
+doc = util.open_yaml(_join(configs_dir, 'airport.SAN', 'preprocessing.yaml'))
 doc['tours']['num_enplanements'] = int(paramByYear.loc[paramByYear['year'] == scenYear, 'airport.SAN.enplanements'].values[0])
 doc['tours']['connecting'] = int(paramByYear.loc[paramByYear['year'] == scenYear, 'airport.SAN.connecting'].values[0])
 doc['tours']['airport_mgra'] = int(paramByYear.loc[paramByYear['year'] == scenYear, 'airport.SAN.airportMgra'].values[0])
-write_yaml(_join(configs_dir, 'airport.SAN', 'preprocessing.yaml'), doc)
-doc = open_yaml(_join(configs_dir, 'airport.SAN', 'trip_mode_choice.yaml'))
+util.write_yaml(_join(configs_dir, 'airport.SAN', 'preprocessing.yaml'), doc)
+doc = util.open_yaml(_join(configs_dir, 'airport.SAN', 'trip_mode_choice.yaml'))
 doc['CONSTANTS']['Taxi_baseFare'] = float(paramByYear.loc[paramByYear.year==scenYear, 'taxi.baseFare'].values[0])
 doc['CONSTANTS']['costPerMile'] = float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.fuel'].values[0]) + float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.maintenance'].values[0])
 doc['CONSTANTS']['Taxi_perMileFare'] = float(paramByYear.loc[paramByYear.year==scenYear, 'taxi.costPerMile'].values[0])
@@ -70,16 +57,16 @@ doc['CONSTANTS']['TNC_shared_baseFare'] = float(paramByYear.loc[paramByYear.year
 doc['CONSTANTS']['TNC_shared_costPerMile'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costPerMile'].values[0])
 doc['CONSTANTS']['TNC_shared_costPerMinute'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costPerMinute'].values[0])
 doc['CONSTANTS']['TNC_shared_costMinimum'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costMinimum'].values[0])
-write_yaml(_join(configs_dir, 'airport.SAN', 'trip_mode_choice.yaml'), doc)
+util.write_yaml(_join(configs_dir, 'airport.SAN', 'trip_mode_choice.yaml'), doc)
 
 #update crossborder parameters
-doc = open_yaml(_join(configs_dir, 'crossborder', 'preprocessing.yaml'))
+doc = util.open_yaml(_join(configs_dir, 'crossborder', 'preprocessing.yaml'))
 doc['tours']['num_tours'] = int(paramByYear.loc[paramByYear.year==scenYear, 'crossBorder.tours'].values[0])
 doc['tours']['pass_shares']['sentri'] = float(paramByYear.loc[paramByYear.year==scenYear, 'crossBorder.sentriShare'].values[0])
 doc['tours']['pass_shares']['ready'] = float(paramByYear.loc[paramByYear.year==scenYear, 'crossBorder.readyShare'].values[0])
 doc['tours']['pass_shares']['no_pass'] = 1-(doc['tours']['pass_shares']['sentri']+doc['tours']['pass_shares']['ready'])
-write_yaml(_join(configs_dir, 'crossborder', 'preprocessing.yaml'), doc)
-doc = open_yaml(_join(configs_dir, 'crossborder', 'trip_mode_choice.yaml'))
+util.write_yaml(_join(configs_dir, 'crossborder', 'preprocessing.yaml'), doc)
+doc = util.open_yaml(_join(configs_dir, 'crossborder', 'trip_mode_choice.yaml'))
 doc['CONSTANTS']['taxi_base_fare'] = float(paramByYear.loc[paramByYear.year==scenYear, 'taxi.baseFare'].values[0])
 doc['CONSTANTS']['cost_per_mile_fuel'] = float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.fuel'].values[0])
 doc['CONSTANTS']['cost_per_mile_maint'] = float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.maintenance'].values[0])
@@ -93,10 +80,10 @@ doc['CONSTANTS']['tnc_shared_base_fare'] = float(paramByYear.loc[paramByYear.yea
 doc['CONSTANTS']['tnc_shared_cost_per_mile'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costPerMile'].values[0])
 doc['CONSTANTS']['tnc_shared_cost_per_minute'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costPerMinute'].values[0])
 doc['CONSTANTS']['tnc_shared_cost_minimum'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costMinimum'].values[0])
-write_yaml(_join(configs_dir, 'crossborder', 'trip_mode_choice.yaml'), doc)
+util.write_yaml(_join(configs_dir, 'crossborder', 'trip_mode_choice.yaml'), doc)
 
 #resident/visitor model parameters
-doc = open_yaml(_join(configs_dir, 'common', 'constants.yaml'))
+doc = util.open_yaml(_join(configs_dir, 'common', 'constants.yaml'))
 doc['scenarioYear'] = int(scenYear)
 doc['costPerMile'] = float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.fuel'].values[0]) + float(paramByYear.loc[paramByYear.year==scenYear, 'aoc.maintenance'].values[0])
 doc['Taxi_baseFare'] = float(paramByYear.loc[paramByYear.year==scenYear, 'taxi.baseFare'].values[0])
@@ -112,11 +99,17 @@ doc['TNC_shared_costPerMinute'] = float(paramByYear.loc[paramByYear.year==scenYe
 doc['TNC_shared_costMinimum'] = float(paramByYear.loc[paramByYear.year==scenYear, 'TNC.shared.costMinimum'].values[0])
 doc['microVarCost'] = float(paramByYear.loc[paramByYear.year==scenYear, 'active.micromobility.variableCost'].values[0]*100) #cents
 doc['microFixedCost'] = float(paramByYear.loc[paramByYear.year==scenYear, 'active.micromobility.fixedCost'].values[0]*100) #cents
-write_yaml(_join(configs_dir, 'common', 'constants.yaml'), doc)
-doc = open_yaml(_join(configs_dir, 'resident', 'av_ownership.yaml'))
+util.write_yaml(_join(configs_dir, 'common', 'constants.yaml'), doc)
+doc = util.open_yaml(_join(configs_dir, 'resident', 'av_ownership.yaml'))
 doc['AV_OWNERSHIP_TARGET_PERCENT'] = float(paramByYear.loc[paramByYear.year==scenYear, 'Mobility.AV.Share'].values[0])
-write_yaml(_join(configs_dir, 'resident', 'av_ownership.yaml'), doc)
+util.write_yaml(_join(configs_dir, 'resident', 'av_ownership.yaml'), doc)
 
-doc = open_yaml(_join(configs_dir, 'resident', 'vehicle_type_choice.yaml'))
+doc = util.open_yaml(_join(configs_dir, 'resident', 'vehicle_type_choice.yaml'))
 doc['FLEET_YEAR'] = int(scenYear)
-write_yaml(_join(configs_dir, 'resident', 'vehicle_type_choice.yaml'), doc)
+util.write_yaml(_join(configs_dir, 'resident', 'vehicle_type_choice.yaml'), doc)
+
+sandag_abm_prop = util.load_properties(sandag_abm_prop_dir)
+doc = util.open_yaml(_join(scripts_dir, 'resident', '2zoneSkim_params.yaml'))
+doc['mmms']['max_maz_local_bus_stop_walk_dist_feet'] = float(sandag_abm_prop['walk.transit.connector.max.length'][0]) * 5280        # converting mile to feet
+doc['mmms']['max_maz_premium_transit_stop_walk_dist_feet'] = float(sandag_abm_prop['walk.transit.connector.max.length'][1]) * 5280  # converting mile to feet
+util.write_yaml(_join(scripts_dir, 'resident', '2zoneSkim_params.yaml'), doc)
