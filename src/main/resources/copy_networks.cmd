6bc7ebeba38c23b833df5695781ae013270579a5
JoeJimFlood <joejimflood@gmail.com>
2024-05-23 08:56:52 -0700

    Added robocopy to copy_networks.cmd to copy geodatabase

--- src/main/resources/copy_networks.cmd
+++ src/main/resources/copy_networks.cmd
@@ -3,11 +3,15 @@
 if "%1"=="" goto usage
 if "%2"=="" goto usage
 
-set FILE_LIST=EMMEOutputs.gdb trlink.csv trrt.csv trstop.csv tap.elev tap.ptype timexfer_EA.csv timexfer_AM.csv timexfer_MD.csv timexfer_PM.csv timexfer_EV.csv special_fares.txt linktypeturns.dbf tapcov.prj tapcov.dbf tapcov.shp tapcov.shx tapcov.shp.xml SANDAG_Bike_Net.sbn SANDAG_Bike_Net.sbx SANDAG_Bike_Net.dbf SANDAG_Bike_Net.shp SANDAG_Bike_Net.shx SANDAG_Bike_Net.prj SANDAG_Bike_Node.sbn SANDAG_Bike_Node.sbx SANDAG_Bike_Node.dbf SANDAG_Bike_Node.shp SANDAG_Bike_Node.shx SANDAG_Bike_Node.prj  
+set FILE_LIST=trlink.csv trrt.csv trstop.csv tap.elev tap.ptype timexfer_EA.csv timexfer_AM.csv timexfer_MD.csv timexfer_PM.csv timexfer_EV.csv special_fares.txt linktypeturns.dbf tapcov.prj tapcov.dbf tapcov.shp tapcov.shx tapcov.shp.xml SANDAG_Bike_Net.sbn SANDAG_Bike_Net.sbx SANDAG_Bike_Net.dbf SANDAG_Bike_Net.shp SANDAG_Bike_Net.shx SANDAG_Bike_Net.prj SANDAG_Bike_Node.sbn SANDAG_Bike_Node.sbx SANDAG_Bike_Node.dbf SANDAG_Bike_Node.shp SANDAG_Bike_Node.shx SANDAG_Bike_Node.prj  
 
 @echo %FILE_LIST%
 
-xcopy /s/Y %1\"*.*" %2
+for %%i in (%FILE_LIST%) do (
+@echo Copying and overwriting %1\%%i
+xcopy /Y %1\%%i %2)
+
+robocopy %1\EMMEOutputs.gdb %2\EMMEOutputs.gdb
 
 goto :eof
 
