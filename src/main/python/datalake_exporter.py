6a29499169ef1711245f643732a0225f5f03ca8f
anneku <aku@sandag.org>
2024-07-25 11:52:57 -0700

    Update datalake_exporter.py

--- src/main/python/datalake_exporter.py
+++ src/main/python/datalake_exporter.py
@@ -230,6 +230,7 @@ def write_to_datalake(output_path, models, exclude, env):
         EMME_metadata["properties_path"],
         os.path.join(output_path, 'skims', 'traffic_skims_MD.omx'),
         os.path.abspath(os.path.join(output_path, '..', 'input', 'zone_term.csv')),
+        os.path.abspath(os.path.join(output_path, '..', 'input', 'trlink.csv')),
         os.path.join(output_path, 'bikeMgraLogsum.csv'),
         os.path.join(output_path, 'microMgraEquivMinutes.csv'),
         os.path.join(report_path, 'walkMgrasWithin45Min_AM.csv'),
