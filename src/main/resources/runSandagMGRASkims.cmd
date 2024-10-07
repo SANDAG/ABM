8406ccda9b635783c082550bb20c01269cc69fc0
Ali Etezady <58451076+aletzdy@users.noreply.github.com>
2024-10-07 07:14:53 -0700

    removing old java related lines from runSandagMGRASkims.cmd

--- src/main/resources/runSandagMGRASkims.cmd
+++ src/main/resources/runSandagMGRASkims.cmd
@@ -21,10 +21,3 @@ cd %PROJECT_DRIVE%%PROJECT_DIRECTORY%
 rem   rem build walk skims
 ECHO Running 2-zone skimming procedure... 
 %PYTHON3% src/asim/scripts/resident/2zoneSkim.py %PROJECT_DIRECTORY% || goto error
-
-@REM python %PROJECT_DRIVE%%PROJECT_DIRECTORY%\python\calculate_micromobility.py  --properties_file %PROJECT_DRIVE%%PROJECT_DIRECTORY%\conf\sandag_abm.properties --outputs_directory %PROJECT_DRIVE%%PROJECT_DIRECTORY%\output --inputs_parent_directory %PROJECT_DRIVE%%PROJECT_DIRECTORY%
-
-:done
-rem ### restore saved environment variable values, and change back to original current directory
-set JAVA_PATH=%OLDJAVAPATH%
-set PATH=%OLDPATH%
