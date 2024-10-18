79ad1dd1220dcc1421011eca7145ecd12e324146
JoeJimFlood <joejimflood@gmail.com>
2024-10-18 08:52:02 -0700

    Removed initial wait time from transit travel time calculations

--- src/main/python/TravelTimeReporter.py
+++ src/main/python/TravelTimeReporter.py
@@ -320,21 +320,18 @@ class TravelTimeReporter:
         print("Calculating transit times")
 
         _loc_time = (
-            self.skims["WALK_LOC_FIRSTWAIT__" + self.settings["time_period"]] +
             self.skims["WALK_LOC_XFERWALK__" + self.settings["time_period"]] +
             self.skims["WALK_LOC_XFERWAIT__" + self.settings["time_period"]] +
             self.skims["WALK_LOC_TOTALIVTT__" + self.settings["time_period"]]
         )
 
         _prm_time = (
-            self.skims["WALK_PRM_FIRSTWAIT__" + self.settings["time_period"]] +
             self.skims["WALK_PRM_XFERWALK__" + self.settings["time_period"]] +
             self.skims["WALK_PRM_XFERWAIT__" + self.settings["time_period"]] +
             self.skims["WALK_PRM_TOTALIVTT__" + self.settings["time_period"]]
         )
 
         _mix_time = (
-            self.skims["WALK_MIX_FIRSTWAIT__" + self.settings["time_period"]] +
             self.skims["WALK_MIX_XFERWALK__" + self.settings["time_period"]] +
             self.skims["WALK_MIX_XFERWAIT__" + self.settings["time_period"]] +
             self.skims["WALK_MIX_TOTALIVTT__" + self.settings["time_period"]]
