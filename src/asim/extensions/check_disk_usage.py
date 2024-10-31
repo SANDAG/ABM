8a9f73a353b743697e34955febf7911c989ce6f5
JoeJimFlood <joejimflood@gmail.com>
2024-10-31 13:48:53 -0700

    Updated check_disk_usage.py to accomodate for removal of activitysim.core.inject

--- src/asim/extensions/check_disk_usage.py
+++ src/asim/extensions/check_disk_usage.py
@@ -4,13 +4,13 @@ import os
 import logging
 import win32com.client as win32
 
-from activitysim.core import inject
+from activitysim.core import workflow
 
 logger = logging.getLogger("activitysim")
 
-@inject.step()
-def check_disk_usage():
-    output_dir = inject.get_injectable("output_dir")
+@workflow.step()
+def check_disk_usage(state):
+    output_dir = state.get_injectable("output_dir")
     path = os.path.abspath(os.path.join(output_dir, "..", ".."))
     disk_usage = win32.Dispatch('Scripting.FileSystemObject').GetFolder(path).Size
     logger.info("Disk space usage: %f GB" % (disk_usage / (1024 ** 3)))
