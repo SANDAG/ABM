f78dae943b0d5c4da776e2f2cb8e8a20400eb072
JiaXu1024 <jx731@nyu.edu>
2025-06-11 14:49:31 -0700

    add updated validation script to python folder

--- src/main/resources/runValidation.bat
+++ src/main/resources/runValidation.bat
@@ -1,17 +1,13 @@
 rem COPY REQUIRED FILES TO SPECIFIED ABM SCENARIO
 @echo off
 
-set SOURCE_DIRECTORY=\\sandag.org\transdata\ABM\release\tools\PowerBI_validation_tool
 set PROJECT_DRIVE=%1
 set PROJECT_DIRECTORY=%2
+set SCENARIOYEAR=%3
 set VALIDATION_DIRECTORY=%PROJECT_DRIVE%%PROJECT_DIRECTORY%\analysis\validation
 
 rem Create Validation Folder If Not Exist
 if not exist %VALIDATION_DIRECTORY% mkdir %VALIDATION_DIRECTORY%
-
-rem ### Copy files using xcopy
-set SCENARIOYEAR=%3
-xcopy %SOURCE_DIRECTORY%\data\%SCENARIOYEAR%\ %VALIDATION_DIRECTORY% /S /E /Y
 
 rem CREATE VIS WORKSHEET FOR POWER BI VISUALIZATIONS
 rem ### Connecting to Anaconda3 Environment
@@ -22,4 +18,4 @@ rem ### Use ABM3 conda env (for both tcov and tned)
 call activate asim_140
 
 rem ### Running validation pipeline for input scenario
-python %SOURCE_DIRECTORY%\src\%SCENARIOYEAR%\validation.py %PROJECT_DRIVE%%PROJECT_DIRECTORY% %SCENARIOYEAR%
+python python\validation.py %PROJECT_DRIVE%%PROJECT_DIRECTORY% %SCENARIOYEAR%
