22c20aceb01e25d340795d8261a81343e44f9f6b
Cundo Arellano <51237056+cundo92@users.noreply.github.com>
2025-07-16 11:51:47 -0700

    Update import_network.py
    Reverted the omission of handling fc 8,9 hov 2,3 links

--- src/main/emme/toolbox/import/import_network.py
+++ src/main/emme/toolbox/import/import_network.py
@@ -575,6 +575,8 @@ class ImportNetwork(_m.Tool(), gen_utils.Snapshot):
                 # managed lanes, free for HOV2 and HOV3+, tolls for SOV
                 if link[toll] > 0:
                     auto_modes =  lookup["TOLL"][link[truck]]
+                elif link.type == 8 or link.type == 9:
+                    auto_modes =  lookup["TOLL"][link[truck]]
                 if link["@hov"] == 2:
                     auto_modes = auto_modes | lookup["HOV2"]
                 else:
