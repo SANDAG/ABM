ade8db74edc735ab33ee30ea7df26792f2b2d629
JiaXu1024 <jx731@nyu.edu>
2026-05-04 10:26:38 -0700

    removed the unavailable import and updated the function call

--- src/asim-cvm/extensions/cvm_terminal_choice.py
+++ src/asim-cvm/extensions/cvm_terminal_choice.py
@@ -7,7 +7,7 @@ from typing import Literal
 import numpy as np
 import pandas as pd
 
-from activitysim.abm.models.util import annotate, tour_destination
+from activitysim.abm.models.util import tour_destination
 from activitysim.core import (
     estimation,
     expressions,
@@ -498,7 +498,12 @@ def route_endpoint(
     state.add_table("routes", routes)
 
     if model_settings.annotate_routes:
-        annotate.annotate_tours(state, model_settings, trace_label)
+        expressions.assign_columns(
+            state,
+            df=routes,
+            model_settings=model_settings.annotate_routes,
+            trace_label=tracing.extend_trace_label(trace_label, "annotate_routes"),
+        )
 
 
 @workflow.step
